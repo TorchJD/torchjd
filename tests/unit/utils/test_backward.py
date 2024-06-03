cea542cc8d54c7131eaa82dc0cd761480705407f
Pierre Quinton <pierre.quinton@epfl.ch>
2024-06-03 08:02:12 +0200

    Add tests for backward

--- tests/unit/utils/test_backward.py
+++ tests/unit/utils/test_backward.py
@@ -94,3 +94,79 @@ def test_backward_grads(aggregator: Aggregator, shape: tuple[int]):
     backward([output], [input], aggregator)
 
     assert_close(input.grad, aggregator(jacobian))
+
+
+def test_backward_empty_inputs():
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+    aggregator = WeightedAggregator(MeanWeighting())
+
+    input = torch.randn(16, 10)  # Batch of 16 input random vectors of length 10
+    target = input.sum(dim=1, keepdim=True)  # Batch of 16 targets
+
+    loss = MSELoss(reduction="none")
+
+    output = model(input)
+    losses = loss(output, target)
+
+    backward([losses], [], aggregator)
+
+    for p in model.parameters():
+        assert p.grad is None
+
+
+def test_backward_partial_inputs():
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+    aggregator = WeightedAggregator(MeanWeighting())
+
+    input = torch.randn(16, 10)  # Batch of 16 input random vectors of length 10
+    target = input.sum(dim=1, keepdim=True)  # Batch of 16 targets
+
+    loss = MSELoss(reduction="none")
+
+    output = model(input)
+    losses = loss(output, target)
+
+    backward([losses], model[0].parameters(), aggregator)
+
+    for p in model[0].parameters():
+        assert (p.grad is not None) and (p.shape == p.grad.shape)
+
+    for p in model[1:].parameters():
+        assert p.grad is None
+
+
+@pytest.mark.parametrize(
+    "chunk_size",
+    [
+        0,
+        -1,
+        -2,
+        -4,
+    ],
+)
+def test_backward_non_positive_chunk_size(chunk_size: int):
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+
+    W = UPGradWrapper(MeanWeighting())
+    A = WeightedAggregator(W)
+
+    input = torch.randn(16, 10)  # Batch of 16 input random vectors of length 10
+    target = input.sum(dim=1, keepdim=True)  # Batch of 16 targets
+
+    loss = MSELoss(reduction="none")
+
+    output = model(input)
+    losses = loss(output, target)
+
+    with pytest.raises(ValueError):
+        backward([losses], model.parameters(), A, parallel_chunk_size=chunk_size)
+
+
+def test_backward_empty_tensors():
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+
+    W = UPGradWrapper(MeanWeighting())
+    A = WeightedAggregator(W)
+
+    with pytest.raises(ValueError):
+        backward([], model.parameters(), A)
