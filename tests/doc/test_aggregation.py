5d50e8795756289351a34117acaea23f115649a9
Valérian Rey <valerian.rey@gmail.com>
2024-06-05 21:59:07 +0200

    Fix formatting

--- tests/doc/test_aggregation.py
+++ tests/doc/test_aggregation.py
@@ -3,11 +3,12 @@ from torch.testing import assert_close
 
 def test_imtlg():
     from torch import tensor
-    from torchjd.aggregation import WeightedAggregator, IMTLGWeighting
+
+    from torchjd.aggregation import IMTLGWeighting, WeightedAggregator
 
     W = IMTLGWeighting()
     A = WeightedAggregator(W)
-    J = tensor([[-4., 1., 1.], [6., 1., 1.]])
+    J = tensor([[-4.0, 1.0, 1.0], [6.0, 1.0, 1.0]])
 
     assert_close(A(J), tensor([0.0767, 1.0000, 1.0000]), rtol=0, atol=1e-4)
     assert_close(W(J), tensor([0.5923, 0.4077]), rtol=0, atol=1e-4)
