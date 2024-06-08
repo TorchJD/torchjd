225422043688e3e614c09c62526f6a1f8199adf1
Valérian Rey <valerian.rey@gmail.com>
2024-06-08 19:56:46 +0200

    Improve tests for chunk_size - Rename one of them - Add None to the list of valid chunk sizes - Remove redundant values - Reformat

--- tests/unit/test_backward.py
+++ tests/unit/test_backward.py
@@ -40,15 +40,8 @@ def test_backward_various_aggregators(aggregator: Aggregator):
         assert (p.grad is not None) and (p.shape == p.grad.shape)
 
 
-@pytest.mark.parametrize(
-    "chunk_size",
-    [
-        1,
-        2,
-        4,
-    ],
-)
-def test_backward_chunk_size(chunk_size):
+@pytest.mark.parametrize("chunk_size", [None, 1, 2, 4])
+def test_backward_valid_chunk_size(chunk_size):
     model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
 
     W = UPGradWrapper(MeanWeighting())
@@ -66,6 +59,25 @@ def test_backward_chunk_size(chunk_size):
 
     for p in model.parameters():
         assert (p.grad is not None) and (p.shape == p.grad.shape)
+
+
+@pytest.mark.parametrize("chunk_size", [0, -1])
+def test_backward_non_positive_chunk_size(chunk_size: int):
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+
+    W = UPGradWrapper(MeanWeighting())
+    A = WeightedAggregator(W)
+
+    input = torch.randn(16, 10)  # Batch of 16 input random vectors of length 10
+    target = input.sum(dim=1, keepdim=True)  # Batch of 16 targets
+
+    loss = MSELoss(reduction="none")
+
+    output = model(input)
+    losses = loss(output, target)
+
+    with pytest.raises(ValueError):
+        backward([losses], model.parameters(), A, parallel_chunk_size=chunk_size)
 
 
 @pytest.mark.parametrize(
@@ -133,33 +145,6 @@ def test_backward_partial_inputs():
 
     for p in model[1:].parameters():
         assert p.grad is None
-
-
-@pytest.mark.parametrize(
-    "chunk_size",
-    [
-        0,
-        -1,
-        -2,
-        -4,
-    ],
-)
-def test_backward_non_positive_chunk_size(chunk_size: int):
-    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
-
-    W = UPGradWrapper(MeanWeighting())
-    A = WeightedAggregator(W)
-
-    input = torch.randn(16, 10)  # Batch of 16 input random vectors of length 10
-    target = input.sum(dim=1, keepdim=True)  # Batch of 16 targets
-
-    loss = MSELoss(reduction="none")
-
-    output = model(input)
-    losses = loss(output, target)
-
-    with pytest.raises(ValueError):
-        backward([losses], model.parameters(), A, parallel_chunk_size=chunk_size)
 
 
 def test_backward_empty_tensors():
