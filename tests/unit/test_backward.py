219636666b48c93452b485dc3008f21bb3b1ef07
Valérian Rey <valerian.rey@gmail.com>
2024-06-15 14:43:35 +0200

    Add test_backward_no_retain_graph_small_chunk_size

--- tests/unit/test_backward.py
+++ tests/unit/test_backward.py
@@ -1,5 +1,8 @@
+from contextlib import nullcontext as does_not_raise
+
 import pytest
 import torch
+from pytest import raises
 from torch.testing import assert_close
 
 from torchjd import backward
@@ -78,6 +81,29 @@ def test_backward_non_positive_chunk_size(chunk_size: int):
 
     with pytest.raises(ValueError):
         backward([y1, y2], params, A, parallel_chunk_size=chunk_size)
+
+
+@pytest.mark.parametrize(
+    ["chunk_size", "expectation"],
+    [(1, raises(ValueError)), (2, does_not_raise()), (None, does_not_raise())],
+)
+def test_backward_no_retain_graph_small_chunk_size(chunk_size: int, expectation):
+    """
+    Tests that backward raises an error when using retain_graph=False and a chunk size that is not
+    large enough to allow differentiation of all tensors are once.
+    """
+
+    A = WeightedAggregator(UPGradWrapper(MeanWeighting()))
+
+    p1 = torch.tensor([1.0, 2.0], requires_grad=True)
+    p2 = torch.tensor([3.0, 4.0], requires_grad=True)
+    params = [p1, p2]
+
+    y1 = torch.tensor([-1.0, 1.0]) @ p1 + p2.sum()
+    y2 = (p1**2).sum() + p2.norm()
+
+    with expectation:
+        backward([y1, y2], params, A, retain_graph=False, parallel_chunk_size=chunk_size)
 
 
 @pytest.mark.parametrize(
