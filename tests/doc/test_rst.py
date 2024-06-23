b5c9a46b68d94d29f2172ceef17cafa8b3d016cc
Pierre Quinton <pierre.quinton@epfl.ch>
2024-06-23 15:10:07 +0200

    Add IWRM usage example (#59)
    * Adds sphinx-design to get side to side cards in documentation
    * Adds a side to side comparison example for IWRM vs ERM.
    * Adds the corresponding test.
    ---------
    
    Co-authored-by: Valérian Rey <valerian.rey@gmail.com>

--- tests/doc/test_rst.py
+++ tests/doc/test_rst.py
@@ -21,3 +21,54 @@ def test_basic_usage():
     optimizer.zero_grad()
     torchjd.backward(losses, model.parameters(), A)
     optimizer.step()
+
+
+def test_iwrm():
+    def test_erm_with_sgd():
+        import torch
+        from torch.nn import Linear, MSELoss, ReLU, Sequential
+        from torch.optim import SGD
+
+        X = torch.randn(8, 16, 10)
+        Y = torch.randn(8, 16, 1)
+
+        model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+        loss_fn = MSELoss()
+
+        params = model.parameters()
+        optimizer = SGD(params, lr=0.1)
+
+        for x, y in zip(X, Y):
+            y_hat = model(x)
+            loss = loss_fn(y_hat, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    def test_iwrm_with_ssjd():
+        import torch
+        from torch.nn import Linear, MSELoss, ReLU, Sequential
+        from torch.optim import SGD
+
+        from torchjd import backward
+        from torchjd.aggregation import UPGrad
+
+        X = torch.randn(8, 16, 10)
+        Y = torch.randn(8, 16, 1)
+
+        model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+        loss_fn = MSELoss(reduction="none")
+
+        params = model.parameters()
+        optimizer = SGD(params, lr=0.1)
+        A = UPGrad()
+
+        for x, y in zip(X, Y):
+            y_hat = model(x)
+            losses = loss_fn(y_hat, y)
+            optimizer.zero_grad()
+            backward(losses, params, A)
+            optimizer.step()
+
+    test_erm_with_sgd()
+    test_iwrm_with_ssjd()
