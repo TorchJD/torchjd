575a29beb2c9306f0e40ca84f3d02e379cc9e6a6
Valérian Rey <valerian.rey@gmail.com>
2024-06-23 14:58:54 +0200

    Add proper test for iwrm usage example

--- tests/doc/test_rst.py
+++ tests/doc/test_rst.py
@@ -23,26 +23,52 @@ def test_basic_usage():
     optimizer.step()
 
 
-def test_ssjd():
-    import torch
-    from torch.nn import Linear, MSELoss, ReLU, Sequential
-    from torch.optim import SGD
+def test_iwrm():
+    def test_erm_with_sgd():
+        import torch
+        from torch.nn import Linear, MSELoss, ReLU, Sequential
+        from torch.optim import SGD
 
-    import torchjd
-    from torchjd.aggregation import UPGrad
+        X = torch.randn(8, 16, 10)
+        Y = torch.randn(8, 16, 1)
 
-    A = UPGrad()
+        model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+        loss_fn = MSELoss()
 
-    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
-    loss_fn = MSELoss(reduction="none")
-    params = model.parameters()
-    optimizer = SGD(params, lr=0.1)
+        params = model.parameters()
+        optimizer = SGD(params, lr=0.1)
 
-    x = torch.randn(16, 10)
-    y = torch.randn(16, 1)
-    y_hat = model(x)
-    losses = loss_fn(y_hat, y)
+        for x, y in zip(X, Y):
+            y_hat = model(x)
+            loss = loss_fn(y_hat, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    optimizer.zero_grad()
-    torchjd.backward(losses, params, A)
-    optimizer.step()
+    def test_iwrm_with_ssjd():
+        import torch
+        from torch.nn import Linear, MSELoss, ReLU, Sequential
+        from torch.optim import SGD
+
+        from torchjd import backward
+        from torchjd.aggregation import UPGrad
+
+        X = torch.randn(8, 16, 10)
+        Y = torch.randn(8, 16, 1)
+
+        model = Sequential(Linear(10, 5), ReLU(), Linear(5, 1))
+        loss_fn = MSELoss(reduction="none")
+
+        params = model.parameters()
+        optimizer = SGD(params, lr=0.1)
+        A = UPGrad()
+
+        for x, y in zip(X, Y):
+            y_hat = model(x)
+            losses = loss_fn(y_hat, y)
+            optimizer.zero_grad()
+            backward(losses, params, A)
+            optimizer.step()
+
+    test_erm_with_sgd()
+    test_iwrm_with_ssjd()
