575281d5418cab4b33a8e10a56b118367cc6edfd
Valérian Rey <valerian.rey@gmail.com>
2024-07-02 00:38:19 +0200

    Improve unit test readability

--- tests/unit/autojac/test_multi_task_backward.py
+++ tests/unit/autojac/test_multi_task_backward.py
@@ -18,28 +18,21 @@ def test_multi_task_backward_various_aggregators(A: Aggregator):
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
     p1 = torch.tensor([1.0, 2.0], requires_grad=True)
     p2 = torch.tensor([3.0, 4.0], requires_grad=True)
-    shared_parameters = [p0]
-    tasks_parameters = [[p1], [p2]]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     multi_task_backward(
-        tasks_losses=tasks_losses,
-        shared_parameters=shared_parameters,
-        shared_representations=shared_representations,
-        tasks_parameters=tasks_parameters,
+        tasks_losses=[y1, y2],
+        shared_parameters=[p0],
+        shared_representations=[r1, r2],
+        tasks_parameters=[[p1], [p2]],
         A=A,
     )
 
-    params = [p0, p1, p2]
-
-    for p in params:
+    for p in [p0, p1, p2]:
         assert (p.grad is not None) and (p.shape == p.grad.shape)
 
 
@@ -54,30 +47,23 @@ def test_multi_task_backward_valid_chunk_size(chunk_size):
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
     p1 = torch.tensor([1.0, 2.0], requires_grad=True)
     p2 = torch.tensor([3.0, 4.0], requires_grad=True)
-    shared_parameters = [p0]
-    tasks_parameters = [[p1], [p2]]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     multi_task_backward(
-        tasks_losses=tasks_losses,
-        shared_parameters=shared_parameters,
-        shared_representations=shared_representations,
-        tasks_parameters=tasks_parameters,
+        tasks_losses=[y1, y2],
+        shared_parameters=[p0],
+        shared_representations=[r1, r2],
+        tasks_parameters=[[p1], [p2]],
         A=A,
         parallel_chunk_size=chunk_size,
         retain_graph=True,
     )
 
-    params = [p0, p1, p2]
-
-    for p in params:
+    for p in [p0, p1, p2]:
         assert (p.grad is not None) and (p.shape == p.grad.shape)
 
 
@@ -92,23 +78,18 @@ def test_multi_task_backward_non_positive_chunk_size(chunk_size: int):
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
     p1 = torch.tensor([1.0, 2.0], requires_grad=True)
     p2 = torch.tensor([3.0, 4.0], requires_grad=True)
-    shared_parameters = [p0]
-    tasks_parameters = [[p1], [p2]]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     with pytest.raises(ValueError):
         multi_task_backward(
-            tasks_losses=tasks_losses,
-            shared_parameters=shared_parameters,
-            shared_representations=shared_representations,
-            tasks_parameters=tasks_parameters,
+            tasks_losses=[y1, y2],
+            shared_parameters=[p0],
+            shared_representations=[r1, r2],
+            tasks_parameters=[[p1], [p2]],
             A=A,
             parallel_chunk_size=chunk_size,
         )
@@ -129,23 +110,18 @@ def test_multi_task_backward_no_retain_graph_small_chunk_size(chunk_size: int, e
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
     p1 = torch.tensor([1.0, 2.0], requires_grad=True)
     p2 = torch.tensor([3.0, 4.0], requires_grad=True)
-    shared_parameters = [p0]
-    tasks_parameters = [[p1], [p2]]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     with expectation:
         multi_task_backward(
-            tasks_losses=tasks_losses,
-            shared_parameters=shared_parameters,
-            shared_representations=shared_representations,
-            tasks_parameters=tasks_parameters,
+            tasks_losses=[y1, y2],
+            shared_parameters=[p0],
+            shared_representations=[r1, r2],
+            tasks_parameters=[[p1], [p2]],
             A=A,
             parallel_chunk_size=chunk_size,
         )
@@ -163,29 +139,27 @@ def test_multi_task_backward_value_is_correct(A: Aggregator, shape: tuple[int]):
     J = torch.randn(shape)
     input = torch.randn([shape[1]], requires_grad=True)
 
-    shared_representation = J @ input
+    r = J @ input
 
     p1 = torch.randn(shape[0], requires_grad=True)
     p2 = torch.randn(shape[0], requires_grad=True)
     p3 = torch.randn(shape[0], requires_grad=True)
-    tasks_parameters = [[p1], [p2], [p3]]
 
-    l1 = p1 @ shared_representation
-    l2 = p2 @ shared_representation
-    l3 = p3 @ shared_representation
-    tasks_losses = [l1, l2, l3]
+    y1 = p1 @ r
+    y2 = p2 @ r
+    y3 = p3 @ r
 
     multi_task_backward(
-        tasks_losses=tasks_losses,
+        tasks_losses=[y1, y2, y3],
         shared_parameters=[input],
-        shared_representations=shared_representation,
-        tasks_parameters=tasks_parameters,
+        shared_representations=r,
+        tasks_parameters=[[p1], [p2], [p3]],
         A=A,
     )
 
-    assert_close(p1.grad, shared_representation)
-    assert_close(p2.grad, shared_representation)
-    assert_close(p3.grad, shared_representation)
+    assert_close(p1.grad, r)
+    assert_close(p2.grad, r)
+    assert_close(p3.grad, r)
 
     resulting_J = torch.stack((p1, p2, p3)) @ J
     assert_close(input.grad, A(resulting_J))
@@ -204,23 +178,18 @@ def test_multi_task_backward_empty_parameters():
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     multi_task_backward(
-        tasks_losses=tasks_losses,
+        tasks_losses=[y1, y2],
         shared_parameters=[],
-        shared_representations=shared_representations,
+        shared_representations=[r1, r2],
         tasks_parameters=[[], []],
         A=A,
     )
 
-    params = [p0, p1, p2]
-
-    for p in params:
+    for p in [p0, p1, p2]:
         assert p.grad is None
 
 
@@ -238,16 +207,13 @@ def test_multi_task_backward_partial_parameters():
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     multi_task_backward(
-        tasks_losses=tasks_losses,
+        tasks_losses=[y1, y2],
         shared_parameters=[p0],
-        shared_representations=shared_representations,
+        shared_representations=[r1, r2],
         tasks_parameters=[[p1], []],
         A=A,
     )
@@ -265,17 +231,15 @@ def test_multi_task_backward_empty_tasks():
     A = UPGrad()
 
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
-    shared_parameters = [p0]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
 
     with pytest.raises(ValueError):
         multi_task_backward(
             tasks_losses=[],
-            shared_parameters=shared_parameters,
-            shared_representations=shared_representations,
+            shared_parameters=[p0],
+            shared_representations=[r1, r2],
             tasks_parameters=[],
             A=A,
         )
@@ -292,32 +256,27 @@ def test_multi_task_backward_incoherent_task_number():
     p0 = torch.tensor([1.0, 2.0], requires_grad=True)
     p1 = torch.tensor([1.0, 2.0], requires_grad=True)
     p2 = torch.tensor([3.0, 4.0], requires_grad=True)
-    shared_parameters = [p0]
-    tasks_parameters = [[p1], [p2]]
 
     r1 = torch.tensor([-1.0, 1.0]) @ p0
     r2 = (p0**2).sum() + p0.norm()
-    shared_representations = [r1, r2]
-
-    l1 = torch.stack((r1 * p1[0], r2 * p1[1]))
-    l2 = r1 * p2[0] + r2 * p2[1]
-    tasks_losses = [l1, l2]
+    y1 = torch.stack((r1 * p1[0], r2 * p1[1]))
+    y2 = r1 * p2[0] + r2 * p2[1]
 
     with pytest.raises(ValueError):
         multi_task_backward(
-            tasks_losses=tasks_losses,
-            shared_parameters=shared_parameters,
-            shared_representations=shared_representations,
-            tasks_parameters=[[p1]],
+            tasks_losses=[y1, y2],
+            shared_parameters=[p0],
+            shared_representations=[r1, r2],
+            tasks_parameters=[[p1]],  # Wrong
             A=A,
             retain_graph=True,
         )
     with pytest.raises(ValueError):
         multi_task_backward(
-            tasks_losses=[l1],
-            shared_parameters=shared_parameters,
-            shared_representations=shared_representations,
-            tasks_parameters=tasks_parameters,
+            tasks_losses=[y1],  # Wrong
+            shared_parameters=[p0],
+            shared_representations=[r1, r2],
+            tasks_parameters=[[p1], [p2]],
             A=A,
             retain_graph=True,
         )
