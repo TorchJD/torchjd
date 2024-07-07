105094d9bded48c09eee640f8d81097521c48676
Valérian Rey <valerian.rey@gmail.com>
2024-07-07 12:58:51 +0200

    Rename some variables in usage examples
    - shared_model > shared_module
    - task*_model > task*_module

--- tests/doc/test_rst.py
+++ tests/doc/test_rst.py
@@ -83,10 +83,10 @@ def test_mtl():
     from torchjd import mtl_backward
     from torchjd.aggregation import UPGrad
 
-    shared_model = Sequential(Linear(10, 5), ReLU(), Linear(5, 3), ReLU())
-    task1_model = Linear(3, 1)
-    task2_model = Linear(3, 1)
-    params = [*shared_model.parameters(), *task1_model.parameters(), *task2_model.parameters()]
+    shared_module = Sequential(Linear(10, 5), ReLU(), Linear(5, 3), ReLU())
+    task1_module = Linear(3, 1)
+    task2_module = Linear(3, 1)
+    params = [*shared_module.parameters(), *task1_module.parameters(), *task2_module.parameters()]
 
     loss_fn = MSELoss()
     optimizer = SGD(params, lr=0.1)
@@ -102,9 +102,9 @@ def test_mtl():
     task2_targets = torch.randn(8, 16, 1)  # 8 batches of 16 targets for second task
 
     for input, target1, target2 in zip(inputs, task1_targets, task2_targets):
-        features = shared_model(input)
-        output1 = task1_model(features)
-        output2 = task2_model(features)
+        features = shared_module(input)
+        output1 = task1_module(features)
+        output2 = task2_module(features)
         loss1 = loss_fn(output1, target1)
         loss2 = loss_fn(output2, target2)
 
@@ -112,8 +112,8 @@ def test_mtl():
         mtl_backward(
             features=features,
             losses=[loss1, loss2],
-            shared_params=shared_model.parameters(),
-            tasks_params=[task1_model.parameters(), task2_model.parameters()],
+            shared_params=shared_module.parameters(),
+            tasks_params=[task1_module.parameters(), task2_module.parameters()],
             A=A,
         )
         optimizer.step()
