a62f602be9d8d5dfe17d0209f41c35d96bc90d9a
Valérian Rey <valerian.rey@gmail.com>
2024-08-31 16:00:52 +0200

    Add missing type hints

--- tests/unit/autojac/_transform/_dict_assertions.py
+++ tests/unit/autojac/_transform/_dict_assertions.py
@@ -2,7 +2,7 @@ from torch import Tensor
 from torch.testing import assert_close
 
 
-def assert_tensor_dicts_are_close(d1: dict[Tensor, Tensor], d2: dict[Tensor, Tensor]):
+def assert_tensor_dicts_are_close(d1: dict[Tensor, Tensor], d2: dict[Tensor, Tensor]) -> None:
     """
     Check that two dictionaries of tensors are close enough. Note that this does not require the
     keys to have the same ordering.
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -17,7 +17,7 @@ if DEVICE == "cuda" and not torch.cuda.is_available():
 
 
 @pytest.fixture(autouse=True)
-def fix_randomness():
+def fix_randomness() -> None:
     rand.seed(0)
     torch.manual_seed(0)
     torch.use_deterministic_algorithms(True)
