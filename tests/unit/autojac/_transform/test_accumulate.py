56331f9f3597d991722c63f9476b6867a6f5195b
Pierre Quinton <pierre.quinton@epfl.ch>
2024-09-02 13:35:09 +0200

    Test multiple calls to `accumulate` without resetting the `.grad` fields

--- tests/unit/autojac/_transform/test_accumulate.py
+++ tests/unit/autojac/_transform/test_accumulate.py
@@ -7,8 +7,11 @@ from torchjd.autojac._transform import Accumulate, Gradients
 from ._dict_assertions import assert_tensor_dicts_are_close
 
 
-def test_accumulate():
-    """Tests that the Accumulate transform correctly accumulates gradients in .grad fields."""
+def test_single_accumulation():
+    """
+    Tests that the Accumulate transform correctly accumulates gradients in .grad fields when run
+    once.
+    """
 
     key1 = torch.zeros([], requires_grad=True, device=DEVICE)
     key2 = torch.zeros([1], requires_grad=True, device=DEVICE)
@@ -27,6 +30,36 @@ def test_accumulate():
 
     grads = {key1: key1.grad, key2: key2.grad, key3: key3.grad}
     expected_grads = {key1: value1, key2: value2, key3: value3}
+
+    assert_tensor_dicts_are_close(grads, expected_grads)
+
+
+@pytest.mark.parametrize("iterations", [1, 2, 4, 10, 13])
+def test_multiple_accumulation(iterations: int):
+    """
+    Tests that the Accumulate transform correctly accumulates gradients in .grad fields when run
+    `iterations` times.
+    """
+
+    key1 = torch.zeros([], requires_grad=True, device=DEVICE)
+    key2 = torch.zeros([1], requires_grad=True, device=DEVICE)
+    key3 = torch.zeros([2, 3], requires_grad=True, device=DEVICE)
+    value1 = torch.ones([], device=DEVICE)
+    value2 = torch.ones([1], device=DEVICE)
+    value3 = torch.ones([2, 3], device=DEVICE)
+    input = Gradients({key1: value1, key2: value2, key3: value3})
+
+    accumulate = Accumulate([key1, key2, key3])
+
+    for i in range(iterations):
+        accumulate(input)
+
+    grads = {key1: key1.grad, key2: key2.grad, key3: key3.grad}
+    expected_grads = {
+        key1: iterations * value1,
+        key2: iterations * value2,
+        key3: iterations * value3,
+    }
 
     assert_tensor_dicts_are_close(grads, expected_grads)
 
