340120b665c5c984abacaf060a1f6fd6c4cf3642
Valérian Rey <31951177+ValerianRey@users.noreply.github.com>
2024-09-16 00:40:27 +0200

    Improve IMTLG clarity (#141)
    * Add comment about usage of lstsq
    * Rename raw_weights to alpha_star
    * Link JD's paper in IMTLG docstrings

--- src/torchjd/aggregation/imtl_g.py
+++ src/torchjd/aggregation/imtl_g.py
@@ -8,7 +8,8 @@ class IMTLG(_WeightedAggregator):
     """
     :class:`~torchjd.aggregation.bases.Aggregator` generalizing the method described in
     `Towards Impartial Multi-task Learning <https://discovery.ucl.ac.uk/id/eprint/10120667/>`_.
-    This generalization supports matrices with some linearly dependant rows.
+    This generalization, defined formally in `Jacobian Descent For Multi-Objective Optimization
+    <https://arxiv.org/pdf/2406.16232>`_, supports matrices with some linearly dependant rows.
 
     .. admonition::
         Example
@@ -31,19 +32,20 @@ class IMTLG(_WeightedAggregator):
 
 class _IMTLGWeighting(_Weighting):
     """
-    :class:`~torchjd.aggregation.bases._Weighting` that extracts weights using a method which is
-    a generalization of the method described in `Towards Impartial Multi-task Learning
-    <https://discovery.ucl.ac.uk/id/eprint/10120667/>`_, supporting non-linearly independent rows
-    of the matrix.
+    :class:`~torchjd.aggregation.bases._Weighting` that extracts weights as described in the
+    definition of A_IMTLG of `Jacobian Descent For Multi-Objective Optimization
+    <https://arxiv.org/pdf/2406.16232>`_.
     """
 
     def forward(self, matrix: Tensor) -> Tensor:
         d = torch.linalg.norm(matrix, dim=1)
 
         try:
-            raw_weights = torch.linalg.lstsq(matrix @ matrix.T, d).solution
+            # Equivalent to `alpha_star = torch.linalg.pinv(matrix @ matrix.T) @ d`, but safer
+            # according to https://pytorch.org/docs/stable/generated/torch.linalg.pinv.html
+            alpha_star = torch.linalg.lstsq(matrix @ matrix.T, d).solution
         except RuntimeError:  # This can happen when the matrix has extremely large values
-            raw_weights = torch.ones(matrix.shape[0], device=matrix.device, dtype=matrix.dtype)
+            alpha_star = torch.ones(matrix.shape[0], device=matrix.device, dtype=matrix.dtype)
 
-        weights = raw_weights / raw_weights.sum()
+        weights = alpha_star / alpha_star.sum()
         return weights
