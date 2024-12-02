3721f2e6b2da8c7fadabfb7844e0655d1dc73c23
Valérian Rey <valerian.rey@gmail.com>
2024-12-02 22:07:11 +0100

    Make excluded param of _get_leaves_of_autograd_graph optional

--- src/torchjd/autojac/backward.py
+++ src/torchjd/autojac/backward.py
@@ -81,7 +81,7 @@ def backward(
     _check_retain_graph_compatible_with_chunk_size(tensors, retain_graph, parallel_chunk_size)
 
     if inputs is None:
-        inputs = _get_leaves_of_autograd_graph(tensors=tensors, excluded=set())
+        inputs = _get_leaves_of_autograd_graph(tensors=tensors)
     else:
         inputs = set(inputs)
 
