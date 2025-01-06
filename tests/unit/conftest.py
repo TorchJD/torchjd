1bcb11457e55c2c77f4d12c8923d9549c8782845
Valérian Rey <valerian.rey@gmail.com>
2025-01-06 21:11:40 +0100

    Change cuda to cuda:0

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -9,11 +9,11 @@ try:
 except KeyError:
     _device_str = "cpu"  # Default to cpu if environment variable not set
 
-if _device_str != "cuda" and _device_str != "cpu":
+if _device_str != "cuda:0" and _device_str != "cpu":
     raise ValueError(f"Invalid value of environment variable PYTEST_TORCH_DEVICE: {_device_str}")
 
-if _device_str == "cuda" and not torch.cuda.is_available():
-    raise ValueError('Requested device "cuda" but cuda is not available.')
+if _device_str == "cuda:0" and not torch.cuda.is_available():
+    raise ValueError('Requested device "cuda:0" but cuda is not available.')
 
 DEVICE = torch.device(_device_str)
 
