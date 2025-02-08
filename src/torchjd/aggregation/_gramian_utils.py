47e845e92501fdb3c36c45eeccfc1914a1b56cf2
Valérian Rey <valerian.rey@gmail.com>
2025-02-08 23:04:28 +0100

    Remove _compute_normalized_gramian
    - Also change _compute_regularized_normalized_gramian into _compute_regularized_gramian

--- src/torchjd/aggregation/_gramian_utils.py
+++ src/torchjd/aggregation/_gramian_utils.py
@@ -1,6 +1,5 @@
 import torch
 from torch import Tensor
-from torch.linalg import LinAlgError
 
 
 def _compute_gramian(matrix: Tensor) -> Tensor:
@@ -11,44 +10,9 @@ def _compute_gramian(matrix: Tensor) -> Tensor:
     return matrix @ matrix.T
 
 
-def _compute_regularized_normalized_gramian(matrix: Tensor, norm_eps: float, reg_eps: float):
-    normalized_gramian = _compute_normalized_gramian(matrix, norm_eps)
-    return _regularize(normalized_gramian, reg_eps)
-
-
-def _compute_normalized_gramian(matrix: Tensor, eps: float) -> Tensor:
-    r"""
-    Computes :math:`\frac{1}{\sigma_\max^2} J J^T` for an input matrix :math:`J`, where
-    :math:`{\sigma_\max^2}` is :math:`J`'s largest singular value.
-    .. hint::
-        :math:`J J^T` is the `Gramian matrix <https://en.wikipedia.org/wiki/Gram_matrix>`_ of
-        :math:`J`
-    For a given matrix :math:`J` with SVD: :math:`J = U S V^T`, we can see that:
-    .. math::
-        \frac{1}{\sigma_\max^2} J J^T = \frac{1}{\sigma_\max^2} U S V^T V S^T U^T = U
-        \left( \frac{S}{\sigma_\max} \right)^2 U^T
-    This is the quantity we compute.
-    .. note::
-        If the provided matrix has dimension :math:`m \times n`, the computation only depends on
-        :math:`n` through the SVD algorithm which is efficient, therefore this is rather fast.
-    """
-
-    try:
-        left_unitary_matrix, singular_values, _ = torch.linalg.svd(matrix, full_matrices=False)
-    except LinAlgError as error:  # Not sure if this can happen
-        raise ValueError(
-            f"Unexpected failure of the svd computation on matrix {matrix}. Please open an "
-            "issue on https://github.com/TorchJD/torchjd/issues and paste this error message in it."
-        ) from error
-    max_singular_value = torch.max(singular_values)
-    if max_singular_value < eps:
-        scaled_singular_values = torch.zeros_like(singular_values)
-    else:
-        scaled_singular_values = singular_values / max_singular_value
-    normalized_gramian = (
-        left_unitary_matrix @ torch.diag(scaled_singular_values**2) @ left_unitary_matrix.T
-    )
-    return normalized_gramian
+def _compute_regularized_gramian(matrix: Tensor, reg_eps: float):
+    gramian = _compute_gramian(matrix)
+    return _regularize(gramian, reg_eps)
 
 
 def _regularize(gramian: Tensor, eps: float) -> Tensor:
