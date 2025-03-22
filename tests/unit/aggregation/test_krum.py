34dcd58587b26587fd4b8f157036e48d722c0b0a
Valérian Rey <valerian.rey@gmail.com>
2025-03-22 20:42:19 +0100

    Add missing Krum tests

--- tests/unit/aggregation/test_krum.py
+++ tests/unit/aggregation/test_krum.py
@@ -1,5 +1,9 @@
-from pytest import mark
+from contextlib import nullcontext as does_not_raise
+
+import torch
+from pytest import mark, raises
 from torch import Tensor
+from unit._utils import ExceptionContext
 
 from torchjd.aggregation import Krum
 
@@ -15,6 +19,58 @@ class TestKrum(ExpectedStructureProperty):
     @mark.parametrize("matrix", scaled_matrices_2_plus_rows + typical_matrices_2_plus_rows)
     def test_expected_structure_property(cls, aggregator: Krum, matrix: Tensor):
         cls._assert_expected_structure_property(aggregator, matrix)
+
+
+@mark.parametrize(
+    ["n_byzantine", "expectation"],
+    [
+        (-5, raises(ValueError)),
+        (-1, raises(ValueError)),
+        (0, does_not_raise()),
+        (1, does_not_raise()),
+        (5, does_not_raise()),
+    ],
+)
+def test_n_byzantine_check(n_byzantine: int, expectation: ExceptionContext):
+    with expectation:
+        _ = Krum(n_byzantine=n_byzantine, n_selected=1)
+
+
+@mark.parametrize(
+    ["n_selected", "expectation"],
+    [
+        (-5, raises(ValueError)),
+        (-1, raises(ValueError)),
+        (0, raises(ValueError)),
+        (1, does_not_raise()),
+        (5, does_not_raise()),
+    ],
+)
+def test_n_selected_check(n_selected: int, expectation: ExceptionContext):
+    with expectation:
+        _ = Krum(n_byzantine=1, n_selected=n_selected)
+
+
+@mark.parametrize(
+    ["n_byzantine", "n_selected", "n_rows", "expectation"],
+    [
+        (1, 1, 3, raises(ValueError)),
+        (1, 1, 4, does_not_raise()),
+        (1, 4, 4, does_not_raise()),
+        (12, 4, 14, raises(ValueError)),
+        (12, 4, 15, does_not_raise()),
+        (12, 15, 15, does_not_raise()),
+        (12, 16, 15, raises(ValueError)),
+    ],
+)
+def test_matrix_shape_check(
+    n_byzantine: int, n_selected: int, n_rows: int, expectation: ExceptionContext
+):
+    aggregator = Krum(n_byzantine=n_byzantine, n_selected=n_selected)
+    matrix = torch.ones([n_rows, 5])
+
+    with expectation:
+        _ = aggregator(matrix)
 
 
 def test_representations():
