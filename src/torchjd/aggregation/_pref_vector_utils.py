7c944dd737fde1e96a7b1a6ed666e11a94c910df
Valérian Rey <valerian.rey@gmail.com>
2025-03-24 17:34:36 +0100

    Move pref_vector validation into _pref_vector_to_weighting

--- src/torchjd/aggregation/_pref_vector_utils.py
+++ src/torchjd/aggregation/_pref_vector_utils.py
@@ -5,17 +5,6 @@ from .bases import _Weighting
 from .constant import _ConstantWeighting
 
 
-def _check_pref_vector(pref_vector: Tensor | None) -> None:
-    """Checks the correctness of the parameter pref_vector."""
-
-    if pref_vector is not None:
-        if pref_vector.ndim != 1:
-            raise ValueError(
-                "Parameter `pref_vector` must be a vector (1D Tensor). Found `pref_vector.ndim = "
-                f"{pref_vector.ndim}`."
-            )
-
-
 def _pref_vector_to_weighting(pref_vector: Tensor | None, default: _Weighting) -> _Weighting:
     """
     Returns the weighting associated to a given preference vector, with a fallback to a default
@@ -25,6 +14,11 @@ def _pref_vector_to_weighting(pref_vector: Tensor | None, default: _Weighting) -
     if pref_vector is None:
         return default
     else:
+        if pref_vector.ndim != 1:
+            raise ValueError(
+                "Parameter `pref_vector` must be a vector (1D Tensor). Found `pref_vector.ndim = "
+                f"{pref_vector.ndim}`."
+            )
         return _ConstantWeighting(pref_vector)
 
 
