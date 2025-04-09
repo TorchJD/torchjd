29cbcb50a8fdfabbe699c42d474baf3911c99fa1
Valérian Rey <valerian.rey@gmail.com>
2025-04-09 14:26:04 +0200

    Update Select to use Set[Tensor] keys

--- src/torchjd/autojac/_transform/select.py
+++ src/torchjd/autojac/_transform/select.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from collections.abc import Set
 
 from torch import Tensor
 
@@ -7,17 +7,18 @@ from .tensor_dict import _A
 
 
 class Select(Transform[_A, _A]):
-    def __init__(self, keys: Iterable[Tensor]):
-        self.keys = set(keys)
+    def __init__(self, keys: Set[Tensor]):
+        self.keys = keys
 
     def __call__(self, tensor_dict: _A) -> _A:
         output = {key: tensor_dict[key] for key in self.keys}
         return type(tensor_dict)(output)
 
     def check_keys(self, input_keys: set[Tensor]) -> set[Tensor]:
-        if not self.keys.issubset(input_keys):
+        keys = set(self.keys)
+        if not keys.issubset(input_keys):
             raise RequirementError(
                 f"The input_keys should be a super set of the keys to select. Found input_keys "
-                f"{input_keys} and keys to select {self.keys}."
+                f"{input_keys} and keys to select {keys}."
             )
-        return self.keys
+        return keys
