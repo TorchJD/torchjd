a26a20d930f66227e29ef9826670330e6b935b20
Valérian Rey <valerian.rey@gmail.com>
2025-04-19 16:04:35 +0200

    Add TestNashMTL
    * This tests the ExpectedStructureProperty on the subset of matrices that NashMTL supports (>1 row, non-zero, not too large)

--- tests/unit/aggregation/test_nash_mtl.py
+++ tests/unit/aggregation/test_nash_mtl.py
@@ -1,4 +1,33 @@
+from pytest import mark
+from torch import Tensor
+
 from torchjd.aggregation import NashMTL
+
+from ._inputs import nashmtl_matrices
+from ._property_testers import ExpectedStructureProperty
+
+
+def _make_aggregator(matrix: Tensor) -> NashMTL:
+    return NashMTL(n_tasks=matrix.shape[0])
+
+
+_aggregators = [_make_aggregator(matrix) for matrix in nashmtl_matrices]
+
+
+@mark.filterwarnings(
+    "ignore:Solution may be inaccurate.",
+    "ignore:You are solving a parameterized problem that is not DPP.",
+)
+class TestNashMTL(ExpectedStructureProperty):
+    # Override the parametrization of `test_expected_structure_property` to make the test use the
+    # right aggregator with each matrix.
+
+    # Note that as opposed to most aggregators, the ExpectedStructureProperty is only tested with
+    # non-scaled matrices, and with matrices of > 1 row. Otherwise, NashMTL fails.
+    @classmethod
+    @mark.parametrize(["aggregator", "matrix"], zip(_aggregators, nashmtl_matrices))
+    def test_expected_structure_property(cls, aggregator: NashMTL, matrix: Tensor):
+        cls._assert_expected_structure_property(aggregator, matrix)
 
 
 def test_representations():
