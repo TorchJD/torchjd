e0009517c2995d1476c5910e9bd2b2360ba878ee
Valérian Rey <valerian.rey@gmail.com>
2025-04-19 16:38:27 +0200

    Add test_nash_mtl_reset

--- tests/unit/aggregation/test_nash_mtl.py
+++ tests/unit/aggregation/test_nash_mtl.py
@@ -1,5 +1,7 @@
+import torch
 from pytest import mark
 from torch import Tensor
+from torch.testing import assert_close
 
 from torchjd.aggregation import NashMTL
 
@@ -28,6 +30,28 @@ class TestNashMTL(ExpectedStructureProperty):
     @mark.parametrize(["aggregator", "matrix"], zip(_aggregators, nashmtl_matrices))
     def test_expected_structure_property(cls, aggregator: NashMTL, matrix: Tensor):
         cls._assert_expected_structure_property(aggregator, matrix)
+
+
+@mark.filterwarnings("ignore: You are solving a parameterized problem that is not DPP.")
+def test_nash_mtl_reset():
+    """
+    Tests that the reset method of NashMTL correctly resets its internal state, by verifying that
+    the result is the same after reset as it right after instantiation.
+
+    To ensure that the aggregations are not all the same, we create different matrices to aggregate.
+    """
+
+    n_aggregations = 4
+    matrices = [torch.randn(3, 5) for _ in range(n_aggregations)]
+
+    aggregator = NashMTL(n_tasks=3, update_weights_every=3)
+    expecteds = [aggregator(matrix) for matrix in matrices]
+
+    aggregator.reset()
+    results = [aggregator(matrix) for matrix in matrices]
+
+    for result, expected in zip(results, expecteds):
+        assert_close(result, expected)
 
 
 def test_representations():
