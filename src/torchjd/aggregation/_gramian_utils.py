9856fc05079bbb3c7c0b61c9cf018e9ac0c63e35
Pierre Quinton <pierre.quinton@gmail.com>
2025-04-29 20:56:18 +0200

    remove regularization util, as not used

--- src/torchjd/aggregation/_gramian_utils.py
+++ src/torchjd/aggregation/_gramian_utils.py
@@ -23,18 +23,3 @@ def normalize(gramian: Tensor, eps: float) -> Tensor:
         return torch.zeros_like(gramian)
     else:
         return gramian / squared_frobenius_norm
-
-
-def regularize(gramian: Tensor, eps: float) -> Tensor:
-    """
-    Adds a regularization term to the gramian to enforce positive definiteness.
-
-    Because of numerical errors, `gramian` might have slightly negative eigenvalue(s). Adding a
-    regularization term which is a small proportion of the identity matrix ensures that the gramian
-    is positive definite.
-    """
-
-    regularization_matrix = eps * torch.eye(
-        gramian.shape[0], dtype=gramian.dtype, device=gramian.device
-    )
-    return gramian + regularization_matrix
