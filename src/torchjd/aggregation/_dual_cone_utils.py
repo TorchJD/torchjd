629ef9634f8169da4f08e396e237b2ec57e2b543
Pierre Quinton <pierre.quinton@gmail.com>
2025-05-01 10:59:23 +0200

    Separate projection of matrix from tensorization.

--- src/torchjd/aggregation/_dual_cone_utils.py
+++ src/torchjd/aggregation/_dual_cone_utils.py
@@ -7,13 +7,36 @@ def project_weights(U: Tensor, G: Tensor, max_iter: int, eps: float) -> Tensor:
     Computes the tensor of weights corresponding to the projection of the vectors in `U` onto the
     rows of a matrix whose Gramian is provided.
 
+    This is a tensorization of _project_weight_matrix
+
+    :param U: The tensor of weights corresponding to the vectors to project, of shape `[..., m]`.
+    :param G: The Gramian matrix of shape `[m, m]`. It must be symmetric and positive definite.
+    :param max_iter: The maximum number of step of projected gradient descent.
+    :param eps: Tolerance precision threshold to stop optimizing.
+    :return: A tensor of projection weights with the same shape as `U`.
+    """
+
+    shape = U.shape
+    m = shape[-1]
+    U_matrix = U.reshape([-1, m]).T
+
+    V = _project_weight_matrix(U_matrix, G, max_iter, eps)
+
+    return V.T.reshape(shape)
+
+
+def _project_weight_matrix(U: Tensor, G: Tensor, max_iter: int, eps: float) -> Tensor:
+    r"""
+    Computes the tensor of weights corresponding to the projection of the columns in `U` onto the
+    rows of a matrix whose Gramian is provided.
+
 
     By Proposition 1 of [1], this is equivalent to solving for `v` the following quadratic program:
 
     minimize        f(v)=\frac{1}{2} v^T G v
     subject to      u \preceq v
 
-    for each u in U.
+    for each column u in U.
 
     This is done by projected gradient descent:
     - Initialize at $v_0=u$.
@@ -29,18 +52,13 @@ def project_weights(U: Tensor, G: Tensor, max_iter: int, eps: float) -> Tensor:
     Reference:
     [1] `Jacobian Descent For Multi-Objective Optimization <https://arxiv.org/pdf/2406.16232>`_.
 
-    :param U: The tensor of weights corresponding to the vectors to project, of shape `[..., m]`.
+    :param U: The matrix of weight vectors corresponding to the vectors to project, of shape
+        `[m, k]`.
     :param G: The Gramian matrix of shape `[m, m]`. It must be symmetric and positive definite.
     :param max_iter: The maximum number of step of projected gradient descent.
     :param eps: Tolerance precision threshold to stop optimizing.
     :return: A tensor of projection weights with the same shape as `U`.
     """
-
-    shape = U.shape
-    m = shape[-1]
-    U_matrix = U.reshape([-1, m]).T
-    V = U_matrix.clone()
-
     driver = "gesvdj" if G.device.type == "cuda" else None
     lambda_max = torch.linalg.svd(G, driver=driver)[1][0]
     if lambda_max < 1e-10:
@@ -48,9 +66,11 @@ def project_weights(U: Tensor, G: Tensor, max_iter: int, eps: float) -> Tensor:
 
     step_size = 1.9 / lambda_max
 
+    V = U.clone()
+
     for t in range(1, max_iter + 1):
-        V_new = torch.maximum(V - step_size * (G @ V), U_matrix)
+        V_new = torch.maximum(V - step_size * (G @ V), U)
         if (V - V_new).norm() < eps:
             break
         V = V_new
-    return V.T.reshape(shape)
+    return V
