613c9bd164269c207581eb728c7f03737a42f08b
Valérian Rey <valerian.rey@gmail.com>
2025-05-13 19:09:11 +0200

    Make upgrad testing params tighter, add rtol

--- tests/unit/aggregation/test_upgrad.py
+++ tests/unit/aggregation/test_upgrad.py
@@ -27,22 +27,22 @@ def test_expected_structure(aggregator: UPGrad, matrix: Tensor):
 
 @mark.parametrize(["aggregator", "matrix"], typical_pairs)
 def test_non_conflicting(aggregator: UPGrad, matrix: Tensor):
-    assert_non_conflicting(aggregator, matrix, atol=4e-04, rtol=0.0)
+    assert_non_conflicting(aggregator, matrix, atol=3e-04, rtol=3e-04)
 
 
 @mark.parametrize(["aggregator", "matrix"], typical_pairs)
 def test_permutation_invariant(aggregator: UPGrad, matrix: Tensor):
-    assert_permutation_invariant(aggregator, matrix, n_runs=5, atol=5e-04, rtol=1e-05)
+    assert_permutation_invariant(aggregator, matrix, n_runs=5, atol=4e-07, rtol=4e-07)
 
 
 @mark.parametrize(["aggregator", "matrix"], typical_pairs)
 def test_linear_under_scaling(aggregator: UPGrad, matrix: Tensor):
-    assert_linear_under_scaling(aggregator, matrix, n_runs=5, atol=3e-02, rtol=0)
+    assert_linear_under_scaling(aggregator, matrix, n_runs=5, atol=3e-02, rtol=3e-02)
 
 
 @mark.parametrize(["aggregator", "matrix"], non_strong_pairs)
 def test_strongly_stationary(aggregator: UPGrad, matrix: Tensor):
-    assert_strongly_stationary(aggregator, matrix, threshold=1e-03)
+    assert_strongly_stationary(aggregator, matrix, threshold=5e-03)
 
 
 @mark.parametrize(["aggregator", "matrix"], requires_grad_pairs)
