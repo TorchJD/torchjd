e17cd60f8a0d87443705626ad13166ce1590a211
Valérian Rey <valerian.rey@gmail.com>
2025-05-26 18:56:49 +0200

    Add docstring to the torchjd package

--- src/torchjd/__init__.py
+++ src/torchjd/__init__.py
@@ -1,1 +1,7 @@
+"""
+This package enable Jacobian descent, through the `backward` and `mtl_backward` functions, which
+are meant to replace the call to `torch.backward` or `loss.backward` in gradient descent. To combine
+the information of the Jacobian, an aggregator from the `aggregation` package has to be used.
+"""
+
 from ._autojac import backward, mtl_backward
