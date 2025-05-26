89424ced8e88d9288082d9c9fefda3187a9baa75
Pierre Quinton <pierre.quinton@gmail.com>
2025-05-26 10:53:43 +0200

    Add excluded tensors

--- src/torchjd/autogram/grad.py
+++ src/torchjd/autogram/grad.py
@@ -5,12 +5,13 @@ from torch.autograd.graph import get_gradient_edge
 from .differentiation_graph import Derivatives, get_jacobian_and_children, topological_sort
 
 
-def grad(outputs: list[Tensor], inputs: set[Tensor]) -> dict[Tensor, Tensor]:
+def grad(outputs: list[Tensor], inputs: set[Tensor], excluded: set[Tensor]) -> dict[Tensor, Tensor]:
     result = {}
     grads = Derivatives([(output, torch.ones_like(output)) for output in outputs])
     roots = [get_gradient_edge(output)[0] for output in outputs]
     leaves = {get_gradient_edge(input)[0]: input for input in inputs}
-    nodes = topological_sort(roots, set(leaves.keys()), set())
+    excluded = {get_gradient_edge(tensor)[0] for tensor in excluded}
+    nodes = topological_sort(roots, set(leaves.keys()), excluded)
     for node in nodes:
         node_grad = grads.pop(node)
         if node in leaves:
