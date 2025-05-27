4758352a619fcffa984078a633927bceb80c9f88
Valérian Rey <valerian.rey@gmail.com>
2025-05-27 14:51:03 +0200

    Fix raise_non_differentiable_error type hint

--- src/torchjd/aggregation/_utils/non_differentiable.py
+++ src/torchjd/aggregation/_utils/non_differentiable.py
@@ -6,5 +6,5 @@ class NonDifferentiableError(RuntimeError):
         super().__init__(f"Trying to differentiate through {module}, which is not differentiable.")
 
 
-def raise_non_differentiable_error(module: nn.Module, _: tuple[Tensor, ...]) -> None:
+def raise_non_differentiable_error(module: nn.Module, _: tuple[Tensor, ...] | Tensor) -> None:
     raise NonDifferentiableError(module)
