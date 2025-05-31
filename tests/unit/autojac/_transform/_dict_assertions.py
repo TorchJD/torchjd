ba206af64b2098fae0d3c2532c6ec5422e1e6cd5
Valérian Rey <valerian.rey@gmail.com>
2025-05-31 18:03:01 +0200

    Add check of structure in assert_tensor_dicts_are_close

--- tests/unit/autojac/_transform/_dict_assertions.py
+++ tests/unit/autojac/_transform/_dict_assertions.py
@@ -1,12 +1,23 @@
 from torch import Tensor
 from torch.testing import assert_close
 
+from torchjd._autojac._transform import TensorDict
+
 
 def assert_tensor_dicts_are_close(d1: dict[Tensor, Tensor], d2: dict[Tensor, Tensor]) -> None:
     """
     Check that two dictionaries of tensors are close enough. Note that this does not require the
     keys to have the same ordering.
+
+    Additionally, if the parameters are TensorDicts, this checks that their content respects their
+    supposed type.
     """
+
+    if isinstance(d1, TensorDict):
+        d1.check()
+
+    if isinstance(d2, TensorDict):
+        d2.check()
 
     assert d1.keys() == d2.keys()
 
