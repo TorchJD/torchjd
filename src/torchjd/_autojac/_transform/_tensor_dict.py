7e942df3e1c7e95511db937541572db916cf15d9
Valérian Rey <valerian.rey@gmail.com>
2025-06-04 00:14:43 +0200

    Fixup __ior__

--- src/torchjd/_autojac/_transform/_tensor_dict.py
+++ src/torchjd/_autojac/_transform/_tensor_dict.py
@@ -37,7 +37,7 @@ class TensorDict(dict[Tensor, Tensor]):
     def _raise_immutable_error(self, *args, **kwargs) -> None:
         raise TypeError(f"{self.__class__.__name__} is immutable.")
 
-    __ior__ = _raise_immutable_error
+    __ior__ = _raise_immutable_error  # type: ignore[assignment]
     __setitem__ = _raise_immutable_error
     __delitem__ = _raise_immutable_error
     clear = _raise_immutable_error
