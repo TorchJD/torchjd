1ca3a7b041c66c69dff05bd0d7505eb537d1958e
Valérian Rey <valerian.rey@gmail.com>
2025-06-07 14:24:54 +0200

    Add second version

--- src/torchjd/autogram/_vgp.py
+++ src/torchjd/autogram/_vgp.py
@@ -7,15 +7,7 @@ from torch import Tensor
 def vgp(func: Callable, *primals) -> tuple[Tensor, Callable]:
     output, vjp_fn = torch.func.vjp(func, *primals)
 
-    def vgp_fn(grad_output: Tensor) -> Tensor:
-        grads = torch.autograd.grad(
-            output,
-            list(primals),
-            grad_outputs=grad_output,
-            retain_graph=True,
-            create_graph=False,
-            allow_unused=False,
-        )
-        return vjp_fn(*grads)[0]
+    def vgp_fn(v: Tensor) -> Tensor:
+        return torch.func.jvp(func, primals, tangents=vjp_fn(v))[1]
 
     return output, vgp_fn
