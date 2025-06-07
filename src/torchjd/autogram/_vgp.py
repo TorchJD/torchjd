b78194d691328b62448034878a127f6b20c91809
Pierre Quinton <pierre.quinton@gmail.com>
2025-06-07 15:49:02 +0200

    Revert "Fix shape of identity matrix given to vgp"
    This reverts commit 2b8c7ecdb1204e96526771eeb08ea3da72b3fcfb.

--- src/torchjd/autogram/_vgp.py
+++ src/torchjd/autogram/_vgp.py
@@ -10,7 +10,7 @@ def get_output_and_gramian(func: Callable, *primals) -> tuple[Tensor, Tensor]:
     def vgp_fn(v: Tensor) -> Tensor:
         return torch.func.jvp(func, primals, tangents=vjp_fn(v))[1]
 
-    identity = torch.eye(output.numel()).reshape([-1] + list(output.shape))
+    identity = torch.eye(output.shape[0])
     gramian = torch.func.vmap(vgp_fn)(identity)
 
     return output, gramian
