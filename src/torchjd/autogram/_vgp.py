4e4f67c50f33a675f2ffb1bf15fbcc9273bc1858
Valérian Rey <valerian.rey@gmail.com>
2025-06-07 14:32:34 +0200

    Replace vgp by get_output_and_gramian

--- src/torchjd/autogram/_vgp.py
+++ src/torchjd/autogram/_vgp.py
@@ -4,10 +4,15 @@ import torch
 from torch import Tensor
 
 
-def vgp(func: Callable, *primals) -> tuple[Tensor, Callable]:
+def get_output_and_gramian(func: Callable, *primals) -> tuple[Tensor, Tensor]:
     output, vjp_fn = torch.func.vjp(func, *primals)
 
     def vgp_fn(v: Tensor) -> Tensor:
         return torch.func.jvp(func, primals, tangents=vjp_fn(v))[1]
 
-    return output, vgp_fn
+    columns = []
+    for e in torch.eye(output.shape[0]):
+        columns.append(vgp_fn(e).unsqueeze(1))
+    gramian = torch.hstack(columns)
+
+    return output, gramian
