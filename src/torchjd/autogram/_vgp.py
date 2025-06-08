96db28000dfd7698f34b5f848b64ea06eb9f78e6
Pierre Quinton <pierre.quinton@gmail.com>
2025-06-08 15:38:47 +0200

    Implements get_gramian from vgp_fn

--- src/torchjd/autogram/_vgp.py
+++ src/torchjd/autogram/_vgp.py
@@ -4,7 +4,7 @@ import torch
 from torch import Tensor
 
 
-def vgp(func: Callable, *primals) -> tuple[Tensor, Callable]:
+def vgp(func: Callable, *primals) -> tuple[Tensor, Callable[[Tensor], Tensor]]:
     output, vjp_fn = torch.func.vjp(func, *primals)
 
     if output.ndim != 1:
@@ -16,10 +16,15 @@ def vgp(func: Callable, *primals) -> tuple[Tensor, Callable]:
     return output, vgp_fn
 
 
+def get_gramian(vgp_fn: Callable[[Tensor], Tensor], m: int) -> Tensor:
+    identity = torch.eye(m)
+    gramian = torch.func.vmap(vgp_fn)(identity)
+
+    return gramian
+
+
 def get_output_and_gramian(func: Callable, *primals) -> tuple[Tensor, Tensor]:
     output, vgp_fn = vgp(func, *primals)
-
-    identity = torch.eye(output.shape[0])
-    gramian = torch.func.vmap(vgp_fn)(identity)
+    gramian = get_gramian(vgp_fn, output.shape[0])
 
     return output, gramian
