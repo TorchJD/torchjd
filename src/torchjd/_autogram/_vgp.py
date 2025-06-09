5b4dc0c8a2e202e90d2b545bc4ae83bbc2f84fbd
Valérian Rey <valerian.rey@gmail.com>
2025-06-09 16:44:37 +0200

    Remove has_aux option from vgp
    * For simplicity for now

--- src/torchjd/_autogram/_vgp.py
+++ src/torchjd/_autogram/_vgp.py
@@ -1,5 +1,4 @@
 from collections.abc import Callable
-from typing import Any
 
 import torch
 from torch import Tensor, nn
@@ -7,14 +6,8 @@ from torch.func import functional_call
 from torch.nn.parameter import Parameter
 
 
-def vgp(
-    func: Callable, *primals, has_aux: bool = False
-) -> tuple[Tensor, Callable[[Tensor], Tensor]] | tuple[Tensor, Callable[[Tensor], Tensor], Any]:
-    if has_aux:
-        output, vjp_fn, aux = torch.func.vjp(func, *primals, has_aux=True)
-    else:
-        output, vjp_fn = torch.func.vjp(func, *primals, has_aux=False)
-        aux = None
+def vgp(func: Callable, *primals) -> tuple[Tensor, Callable[[Tensor], Tensor]]:
+    output, vjp_fn = torch.func.vjp(func, *primals, has_aux=False)
 
     if output.ndim != 1:
         raise ValueError("The function should return a vector")
@@ -22,10 +15,7 @@ def vgp(
     def vgp_fn(v: Tensor) -> Tensor:
         return torch.func.jvp(func, primals, tangents=vjp_fn(v))[1]
 
-    if has_aux:
-        return output, vgp_fn, aux
-    else:
-        return output, vgp_fn
+    return output, vgp_fn
 
 
 def get_gramian(vgp_fn: Callable[[Tensor], Tensor], output: Tensor) -> Tensor:
