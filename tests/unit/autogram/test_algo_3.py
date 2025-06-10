1ea4b2d897749a5f7b8b9c51af5911a4dd6cc55b
Valérian Rey <valerian.rey@gmail.com>
2025-06-10 02:31:58 +0200

    [WIP] Remove vmap, add autojac comparison, etc

--- tests/unit/autogram/test_algo_3.py
+++ tests/unit/autogram/test_algo_3.py
@@ -1,58 +1,98 @@
+import time
+
 import torch
-from torch import Tensor, nn, vmap
+from torch import Tensor, nn
 from torch.nn import ReLU
+
+from torchjd._autojac._transform import Diagonalize, Init, Jac, OrderedSet
+from torchjd._autojac._transform._aggregate import _Matrixify
+
+torch.set_default_device("cpu")
 
 
 def test_algo_3():
+    torch.use_deterministic_algorithms(False)
+
     layers = [
-        nn.Conv2d(3, 32, 3, bias=False),
+        nn.Conv2d(3, 32, 3),
         ReLU(),
-        nn.Conv2d(32, 64, 3, groups=32, bias=False),
+        nn.Conv2d(32, 64, 3, groups=32),
         nn.MaxPool2d(2),
         ReLU(),
-        nn.Conv2d(64, 64, 3, groups=64, bias=False),
+        nn.Conv2d(64, 64, 3, groups=64),
         nn.MaxPool2d(3),
         ReLU(),
         nn.Flatten(),
-        nn.Linear(1024, 128, bias=False),
+        nn.Linear(1024, 128),
         ReLU(),
-        nn.Linear(128, 1, bias=False),
+        nn.Linear(128, 1),
         nn.Flatten(start_dim=0),
     ]
-    batch_size = 16
+    batch_size = 64
     input_shape = (batch_size, 3, 32, 32)
     activation = torch.randn(input_shape)
+    target = torch.arange(0, batch_size, dtype=torch.float32)
+    criterion = torch.nn.MSELoss(reduction="none")
 
     activations = [activation]
     for layer in layers:
         activation = layer(activation)
         activations.append(activation)
 
+    losses = criterion(activation, target)
+    activations.append(losses)
+
+    start = time.perf_counter()
     grad = torch.ones_like(activations[-1])
     gramian = torch.zeros(batch_size, batch_size)
     for i, (input, output, layer) in list(
-        enumerate(zip(activations[:-1], activations[1:], layers))
+        enumerate(zip(activations[:-1], activations[1:], layers + [criterion]))
     )[::-1]:
-        print("i: ", i)
         params = list(layer.parameters())
         if len(params) > 0:
 
             def get_vjp(x):
                 return torch.autograd.grad(output, params, x, retain_graph=True)
 
-            diagonalized_grad = diagonalize(grad)
-            jacobians = vmap(get_vjp)(diagonalized_grad)
+            # diagonalized_grad = diagonalize(grad)
+            j_rows = []
+            for j in range(len(grad)):
+                g = diagonalize_one_row(grad, j)
+                j_rows.append(torch.concatenate([jrow.flatten() for jrow in get_vjp(g)]))
 
-            for jacobian in jacobians:
-                jacobian = jacobian.reshape((batch_size, -1))
-                G = jacobian @ jacobian.T
-                print("G: ", G)
-                gramian += G
+            jacobian = torch.stack(j_rows)
+            gramian += jacobian @ jacobian.T
+
+            # jacobians = vmap(get_vjp, chunk_size=1)(diagonalized_grad)
+
+            # for jacobian in jacobians:
+            #     jacobian = jacobian.reshape((batch_size, -1))
+            #     gramian += jacobian @ jacobian.T  # Accumulate the gramian
 
         if i == 0:
-            break
+            break  # Don't try to differentiate with respect to the model's input
         grad = torch.autograd.grad(output, input, grad, retain_graph=True)[0]
-        # print("gramian: ", gramian)
+
+    end = time.perf_counter()
+    print(end - start)
+    print("gramian (autogram): ", gramian)
+
+    start = time.perf_counter()
+
+    params = OrderedSet(nn.Sequential(*layers).parameters())
+    output = OrderedSet([activations[-1]])
+
+    init = Init(output)
+    diag = Diagonalize(output)
+    jac = Jac(output, params, chunk_size=1)
+    matrixify = _Matrixify()
+    transform = matrixify << jac << diag << init
+    jacobian_matrices = transform({})
+    gramian = torch.stack([J @ J.T for J in jacobian_matrices.values()]).sum(dim=0)
+
+    end = time.perf_counter()
+    print(end - start)
+    print("gramian (autojac): ", gramian)
 
 
 def test_diagonalize():
@@ -69,3 +109,29 @@ def diagonalize(t: Tensor) -> Tensor:
         d[i, i] = row
 
     return d
+
+
+def diagonalize_one_row(t: Tensor, i: int) -> Tensor:
+    d = torch.zeros_like(t)
+    d[i] = t[i]
+
+    return d
+
+
+def test_diagonalize_one_row_sparse():
+    t = torch.randn(10, 5)
+    print(diagonalize_one_row_sparse(t, 0))
+    print(diagonalize_one_row_sparse(t, 1))
+    print(diagonalize_one_row_sparse(t, 2))
+    print(diagonalize_one_row_sparse(t, 3))
+
+
+def diagonalize_one_row_sparse(x: Tensor, index: int = 0) -> Tensor:
+    """
+    Returns a sparse tensor with only the slice at `index` along dim=0 kept, in COO format.
+    Works for x.ndim >= 1 of any shape.
+    """
+    dense = torch.zeros_like(x)
+    dense[index] = x[index]
+    sparse = dense.to_sparse()  # default is sparse_coo
+    return sparse
