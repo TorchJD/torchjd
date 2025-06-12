c582f607bc64e0cb9a3065fabaa782072d107101
Valérian Rey <valerian.rey@gmail.com>
2025-06-12 18:54:08 +0200

    Remove costly inputs

--- tests/unit/aggregation/_inputs.py
+++ tests/unit/aggregation/_inputs.py
@@ -72,8 +72,5 @@ _dnq_upgrad_dims = [
     (512, 100, 100),
     (1024, 100, 100),
     (2048, 100, 100),
-    (4096, 100, 100),
-    (8192, 100, 100),
-    (16384, 100, 100),
 ]
 dnq_upgrad_matrices = [NormalSampler(m, n, r)(_rng) for m, n, r in _dnq_upgrad_dims]
