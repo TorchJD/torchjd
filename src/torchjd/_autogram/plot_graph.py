b49a496dfa011779030ac9d39269b3c6f839436e
Valérian Rey <valerian.rey@gmail.com>
2025-07-12 21:44:48 +0200

    Add FlatNonSequentialNN in plot_graph.py

--- src/torchjd/_autogram/plot_graph.py
+++ src/torchjd/_autogram/plot_graph.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn
+from torch import Tensor, nn
 from torchviz import make_dot
 
 
@@ -19,6 +19,23 @@ class Cifar10Model(nn.Sequential):
         super().__init__(*layers)
 
 
+class FlatNonSequentialNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.fc0 = nn.Linear(9, 10)
+        self.fc1 = nn.Linear(10, 12)
+        self.fc2 = nn.Linear(12, 15)
+        self.fc3 = nn.Linear(10, 15)
+
+    def forward(self, input: Tensor) -> Tensor:
+        common_input = self.relu(self.fc0(input))
+        branch1 = self.fc2(self.relu(self.fc1(common_input)))
+        branch2 = self.fc3(common_input)
+        output = branch1 + branch2
+        return output
+
+
 def main():
     batch_size = 64
     input_shape = (batch_size, 3, 32, 32)
@@ -35,5 +52,17 @@ def main():
     graph.view()
 
 
+def main2():
+    batch_size = 64
+    input_shape = (batch_size, 9)
+    input = torch.randn(input_shape)
+
+    model = FlatNonSequentialNN()
+    output = model(input)
+
+    graph = make_dot(output, params=dict(model.named_parameters()))
+    graph.view()
+
+
 if __name__ == "__main__":
-    main()
+    main2()
