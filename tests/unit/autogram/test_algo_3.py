0279fd06be26764c1f44ed92ad81a7bfe8700d99
Valérian Rey <valerian.rey@gmail.com>
2025-07-29 16:00:16 +0200

    Remove test_equivalence2 (it was actually testing exactly the same thing as test_equivalence)

--- tests/unit/autogram/test_algo_3.py
+++ tests/unit/autogram/test_algo_3.py
@@ -356,50 +356,6 @@ def test_equivalence(model: nn.Module, single_input_shape: tuple[int, ...]):
     assert_tensor_dicts_are_close(grads, expected_grads)
 
 
-@mark.parametrize(
-    ["model", "single_input_shape"],
-    [
-        (Cifar10Model(), (3, 32, 32)),
-        (FlatNonSequentialNN(), (9,)),
-        (SingleInputSingleOutputModel(), (50,)),
-        (SingleInputSingleOutputModel2(), (50,)),
-        (PyTreeModel(), (50,)),
-        (ModuleWithParameterReuse(), (50,)),
-        (ModelWithInterModuleParameterReuse(), (50,)),
-        (ModelWithModuleReuse(), (50,)),
-        (ModelWithFreeParameter(), (15,)),
-        (ModelWithNoFreeParameter(), (15,)),
-        (ModuleWithUnusedParam(), (50,)),
-        (ModuleWithFrozenParam(), (50,)),
-    ],
-)
-def test_equivalence2(model: nn.Module, single_input_shape: tuple[int, ...]):
-    batch_size = 64
-    input_shape = (batch_size,) + single_input_shape
-    input = randn_(input_shape)
-    target = randint_(0, 10, (batch_size,))
-
-    model = model.to(device=DEVICE)
-    criterion = torch.nn.CrossEntropyLoss(reduction="none")
-
-    A = UPGrad()
-    W = A.weighting.weighting
-
-    autojac_forward_backward(model, criterion, input, target, A)
-    expected_grads = {p: p.grad for p in model.parameters() if p.grad is not None}
-    model.zero_grad()
-
-    # autogram backward
-    augment_model_with_iwrm_autogram(model, W)
-    output = model(input)
-    losses = criterion(output, target)
-    losses.backward(torch.ones_like(losses))
-
-    grads = {p: p.grad for p in model.parameters() if p.grad is not None}
-
-    assert_tensor_dicts_are_close(grads, expected_grads)
-
-
 def noop():
     pass
 
