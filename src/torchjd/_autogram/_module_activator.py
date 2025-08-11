da4e83144cfbca76fe3bb256db42ce827f565ed1
Pierre Quinton <pierre.quinton@gmail.com>
2025-08-11 10:51:03 +0200

    Make `HookActivator` have the responsibility of handling the activation of hooks.

---
+++ src/torchjd/_autogram/_module_activator.py
@@ -0,0 +1,29 @@
+from collections.abc import Callable
+
+from torch import nn
+from torch.utils._pytree import PyTree
+
+
+class HookActivator:
+    """
+    This class converts module hooks into hooks that can be activated or deactivate.
+    """
+
+    def __init__(self):
+        self.is_active = True
+
+    def activate(self) -> None:
+        self.is_active = True
+
+    def deactivate(self) -> None:
+        self.is_active = False
+
+    def convert_hook(
+        self, hook: Callable[[nn.Module, PyTree, PyTree], PyTree]
+    ) -> Callable[[nn.Module, PyTree, PyTree], PyTree]:
+        def activated_hook(module: nn.Module, args: PyTree, output: PyTree):
+            if not self.is_active:
+                return output
+            return hook(module, args, output)
+
+        return activated_hook
