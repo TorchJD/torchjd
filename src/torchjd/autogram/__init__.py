a52a4f71821d91d0c58617754e59db2bbab0d9e1
Valérian Rey <valerian.rey@gmail.com>
2025-08-15 17:00:34 +0200

    Fix formatting

--- src/torchjd/autogram/__init__.py
+++ src/torchjd/autogram/__init__.py
@@ -1,6 +1,6 @@
 """
 This package enables Jacobian descent by reverse Gramian accumulation through the
-`augment_model_for_iwrm` function.
+``augment_model_for_iwrm`` function.
 """
 
 from ._augment_model import augment_model_for_iwrm
--- src/torchjd/autojac/__init__.py
+++ src/torchjd/autojac/__init__.py
@@ -1,7 +1,8 @@
 """
-This package enable Jacobian descent, through the `backward` and `mtl_backward` functions, which
-are meant to replace the call to `torch.backward` or `loss.backward` in gradient descent. To combine
-the information of the Jacobian, an aggregator from the `aggregation` package has to be used.
+This package enable Jacobian descent, through the ``backward`` and ``mtl_backward`` functions, which
+are meant to replace the call to ``torch.backward`` or ``loss.backward`` in gradient descent. To
+combine the information of the Jacobian, an aggregator from the ``aggregation`` package has to be
+used.
 """
 
 from ._backward import backward
