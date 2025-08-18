d5a802dc26135f9bbe590b5efff6ed074e3ffe16
Valérian Rey <valerian.rey@gmail.com>
2025-08-18 16:11:23 +0200

    Add test for the import error when trying to import something that does not exist from torchjd

--- tests/unit/test_deprecations.py
+++ tests/unit/test_deprecations.py
@@ -2,12 +2,12 @@ import pytest
 
 
 # deprecated since 2025-08-18
-def test_deprecate_import_backward_from_torchjd():
+def test_deprecate_imports_from_torchjd():
     with pytest.deprecated_call():
         from torchjd import backward  # noqa: F401
 
-
-# deprecated since 2025-08-18
-def test_deprecate_import_mtl_backward_from_torchjd():
     with pytest.deprecated_call():
         from torchjd import mtl_backward  # noqa: F401
+
+    with pytest.raises(ImportError):
+        from torchjd import something_that_does_not_exist  # noqa: F401
