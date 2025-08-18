8f05e4287415704ee38a1e13acec9562ca909373
Pierre Quinton <pierre.quinton@gmail.com>
2025-08-18 15:53:09 +0200

    fix link to autojac in documentation

--- src/torchjd/autogram/__init__.py
+++ src/torchjd/autogram/__init__.py
@@ -4,8 +4,8 @@ described in Section 6 of `Jacobian Descent For Multi-Objective Optimization
 <https://arxiv.org/pdf/2406.16232>`_). It provides a convenient way to modify a model's backward
 pass, allowing you to seamlessly integrate multi-objective optimization in your PyTorch code.
 
-This method typically provides a memory improvement over the :doc:`~torchjd.autojac` package which
-typically leads to time improvement.
+This method typically provides a memory improvement over the :doc:`autojac <../autojac/index>`
+package which typically leads to time improvement.
 """
 
 from ._augment_model import augment_model_for_iwrm
