68e760f009fb1b5d8233417831e53ef0d95eaea6
Pierre Quinton <pierre.quinton@gmail.com>
2025-08-24 14:09:09 +0200

    Improve the example in `test_bacward_vmap` to ressemble more the structure of the test that fails when run with the engine. Now the model is exaclty the same, we hook modules with a hook that add a Node, this node's backward is a hand made function in which we defined vmap. This is interesting as this works. The path of differentiation really goes to the `vmap` function as it should. Now I don't understand why this doesn't happen in the engin/module_hook_manager as essentially everything is the same.

--- tests/unit/autogram/test_backward_vmap.py
+++ tests/unit/autogram/test_backward_vmap.py
@@ -1,5 +1,9 @@
 import torch
-from torch import Tensor, vmap
+from torch import Tensor, nn, vmap
+from torch.autograd.graph import get_gradient_edge
+from torch.utils._pytree import PyTree, tree_map
+
+from torchjd.autogram._edge_registry import EdgeRegistry
 
 
 class backward_fn(torch.autograd.Function):
@@ -32,13 +36,36 @@ class test_fn(torch.autograd.Function):
         return t
 
 
-def test_backward_vmap():
-    a = torch.randn([3], requires_grad=True)
-    b = torch.randn([3])
+def test_non_batched():
+    # This is an adaptation of basic example using autogram.
+    import torch
+    from torch.nn import Linear, MSELoss, ReLU, Sequential
 
-    c = test_fn.apply(a + b)
+    model = Sequential(Linear(10, 5), ReLU(), Linear(5, 2))
+    edge_registry = EdgeRegistry()
+
+    def module_hook(_: nn.Module, __, output: PyTree) -> PyTree:
+        edge_registry.register(get_gradient_edge(output))
+        return tree_map(lambda x: test_fn.apply(x), output)
+
+    for module in model.modules():
+        if any(True for _ in module.parameters(recurse=False)):
+            module.register_forward_hook(module_hook)
+
+    input = torch.randn(16, 10)  # Batch of 16 random input vectors of length 10
+    target1 = torch.randn(16)  # First batch of 16 targets
+    target2 = torch.randn(16)  # Second batch of 16 targets
+
+    loss_fn = MSELoss()
+    output = model(input)
+    output = test_fn.apply(output)
+    loss1 = loss_fn(output[:, 0], target1)
+    loss2 = loss_fn(output[:, 1], target2)
+    losses = torch.stack([loss1, loss2])
+
+    leaves = list(edge_registry.get_leaf_edges({get_gradient_edge(losses)}, set()))
 
     def differentiation(grads):
-        return torch.autograd.grad(c, a, grad_outputs=grads)
+        return torch.autograd.grad(losses, leaves, grad_outputs=grads)
 
-    vmap(differentiation)(torch.diag(torch.ones_like(c)))
+    vmap(differentiation)(torch.diag(torch.ones_like(losses)))
