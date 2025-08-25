a279370b2bcb082424a8db21640b0e430eb656f1
Valérian Rey <valerian.rey@gmail.com>
2025-08-25 18:21:48 +0200

    Fix imports of CAGrad and NashMTL in test_values.py

--- tests/unit/aggregation/test_values.py
+++ tests/unit/aggregation/test_values.py
@@ -10,8 +10,6 @@ from torchjd.aggregation import (
     Aggregator,
     AlignedMTL,
     AlignedMTLWeighting,
-    CAGrad,
-    CAGradWeighting,
     ConFIG,
     Constant,
     ConstantWeighting,
@@ -24,7 +22,6 @@ from torchjd.aggregation import (
     Mean,
     MeanWeighting,
     MGDAWeighting,
-    NashMTL,
     PCGrad,
     PCGradWeighting,
     Random,
@@ -57,7 +54,6 @@ J_TrimmedMean = tensor(
 
 AGGREGATOR_PARAMETRIZATIONS = [
     (AlignedMTL(), J_base, tensor([0.2133, 0.9673, 0.9673])),
-    (CAGrad(c=0.5), J_base, tensor([0.1835, 1.2041, 1.2041])),
     (ConFIG(), J_base, tensor([0.1588, 2.0706, 2.0706])),
     (Constant(tensor([1.0, 2.0])), J_base, tensor([8.0, 3.0, 3.0])),
     (DualProj(), J_base, tensor([0.5563, 1.1109, 1.1109])),
@@ -66,7 +62,6 @@ AGGREGATOR_PARAMETRIZATIONS = [
     (Krum(n_byzantine=1, n_selected=4), J_Krum, tensor([1.2500, 0.7500, 1.5000])),
     (Mean(), J_base, tensor([1.0, 1.0, 1.0])),
     (MGDA(), J_base, tensor([0.0, 1.0, 1.0])),
-    (NashMTL(n_tasks=2), J_base, tensor([0.0542, 0.7061, 0.7061])),
     (PCGrad(), J_base, tensor([0.5848, 3.8012, 3.8012])),
     (Random(), J_base, tensor([-2.6229, 1.0000, 1.0000])),
     (Sum(), J_base, tensor([2.0, 2.0, 2.0])),
@@ -74,23 +69,11 @@ AGGREGATOR_PARAMETRIZATIONS = [
     (UPGrad(), J_base, tensor([0.2929, 1.9004, 1.9004])),
 ]
 
-
-@mark.parametrize(["A", "J", "expected_output"], AGGREGATOR_PARAMETRIZATIONS)
-def test_aggregator_output(A: Aggregator, J: Tensor, expected_output: Tensor):
-    """Test that the output values of an aggregator are fixed (on cpu)."""
-
-    if isinstance(A, NashMTL):
-        warnings.filterwarnings("ignore")
-
-    assert_close(A(J), expected_output, rtol=0, atol=1e-4)
-
-
 G_base = J_base @ J_base.T
 G_Krum = J_Krum @ J_Krum.T
 
 WEIGHTING_PARAMETRIZATIONS = [
     (AlignedMTLWeighting(), G_base, tensor([0.5591, 0.4083])),
-    (CAGradWeighting(c=0.5), G_base, tensor([0.7041, 0.5000])),
     (ConstantWeighting(tensor([1.0, 2.0])), G_base, tensor([1.0, 2.0])),
     (DualProjWeighting(), G_base, tensor([0.6109, 0.5000])),
     (IMTLGWeighting(), G_base, tensor([0.5923, 0.4077])),
@@ -102,6 +85,34 @@ WEIGHTING_PARAMETRIZATIONS = [
     (SumWeighting(), G_base, tensor([1.0, 1.0])),
     (UPGradWeighting(), G_base, tensor([1.1109, 0.7894])),
 ]
+
+try:
+    from torchjd.aggregation import CAGrad, CAGradWeighting
+
+    AGGREGATOR_PARAMETRIZATIONS.append((CAGrad(c=0.5), J_base, tensor([0.1835, 1.2041, 1.2041])))
+    WEIGHTING_PARAMETRIZATIONS.append((CAGradWeighting(c=0.5), G_base, tensor([0.7041, 0.5000])))
+except ImportError:
+    pass
+
+try:
+    from torchjd.aggregation import NashMTL
+
+    AGGREGATOR_PARAMETRIZATIONS.append(
+        (NashMTL(n_tasks=2), J_base, tensor([0.0542, 0.7061, 0.7061]))
+    )
+
+except ImportError:
+    pass
+
+
+@mark.parametrize(["A", "J", "expected_output"], AGGREGATOR_PARAMETRIZATIONS)
+def test_aggregator_output(A: Aggregator, J: Tensor, expected_output: Tensor):
+    """Test that the output values of an aggregator are fixed (on cpu)."""
+
+    if isinstance(A, NashMTL):
+        warnings.filterwarnings("ignore")
+
+    assert_close(A(J), expected_output, rtol=0, atol=1e-4)
 
 
 @mark.parametrize(["W", "G", "expected_output"], WEIGHTING_PARAMETRIZATIONS)
