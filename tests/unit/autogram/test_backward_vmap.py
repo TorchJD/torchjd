f5341ca80e24188a822970d2502fbbf0e812bbd5
Pierre Quinton <pierre.quinton@gmail.com>
2025-08-25 08:38:51 +0200

    Improve the fake example `test_non_batched` in `test_backward_vmap`. Now uses `ModuleHookManager` to hook modules. The backward pass seems to take the vmap route so the problem probably is in the engine.

--- tests/unit/autogram/test_backward_vmap.py
+++ tests/unit/autogram/test_backward_vmap.py
@@ -1,39 +1,9 @@
-import torch
-from torch import Tensor, nn, vmap
+from torch import vmap
 from torch.autograd.graph import get_gradient_edge
-from torch.utils._pytree import PyTree, tree_map
 
 from torchjd.autogram._edge_registry import EdgeRegistry
-
-
-class backward_fn(torch.autograd.Function):
-    @staticmethod
-    def forward(t: Tensor) -> None:
-        return None
-
-    @staticmethod
-    def setup_context(*_):
-        pass
-
-    @staticmethod
-    def vmap(info, in_dims: tuple[int | None], t: Tensor) -> tuple[None, None]:
-        return None, None
-
-
-class test_fn(torch.autograd.Function):
-
-    @staticmethod
-    def forward(t: Tensor) -> Tensor:
-        return t
-
-    @staticmethod
-    def setup_context(*_):
-        pass
-
-    @staticmethod
-    def backward(ctx, t: Tensor) -> Tensor:
-        backward_fn.apply(t)
-        return t
+from torchjd.autogram._gramian_accumulator import GramianAccumulator
+from torchjd.autogram._module_hook_manager import ModuleHookManager
 
 
 def test_non_batched():
@@ -43,14 +13,13 @@ def test_non_batched():
 
     model = Sequential(Linear(10, 5), ReLU(), Linear(5, 2))
     edge_registry = EdgeRegistry()
+    gramian_accumulator = GramianAccumulator()
 
-    def module_hook(_: nn.Module, __, output: PyTree) -> PyTree:
-        edge_registry.register(get_gradient_edge(output))
-        return tree_map(lambda x: test_fn.apply(x), output)
+    module_hook_manager = ModuleHookManager(edge_registry, gramian_accumulator)
 
     for module in model.modules():
         if any(True for _ in module.parameters(recurse=False)):
-            module.register_forward_hook(module_hook)
+            module_hook_manager.hook_module(module)
 
     input = torch.randn(16, 10)  # Batch of 16 random input vectors of length 10
     target1 = torch.randn(16)  # First batch of 16 targets
@@ -58,10 +27,11 @@ def test_non_batched():
 
     loss_fn = MSELoss()
     output = model(input)
-    output = test_fn.apply(output)
     loss1 = loss_fn(output[:, 0], target1)
     loss2 = loss_fn(output[:, 1], target2)
     losses = torch.stack([loss1, loss2])
+
+    module_hook_manager.gramian_accumulation_phase = True
 
     leaves = list(edge_registry.get_leaf_edges({get_gradient_edge(losses)}, set()))
 
