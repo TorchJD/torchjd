2eaab5a1d51eab49f3bc4f253d745b4471fa9150
Pierre Quinton <pierre.quinton@epfl.ch>
2025-08-29 08:46:42 +0200

    Update src/torchjd/autojac/__init__.py
    Co-authored-by: Valérian Rey <31951177+ValerianRey@users.noreply.github.com>

--- src/torchjd/autojac/__init__.py
+++ src/torchjd/autojac/__init__.py
@@ -1,5 +1,5 @@
 """
-This package enable Jacobian descent, through the ``backward`` and ``mtl_backward`` functions, which
+This package enables Jacobian descent, through the ``backward`` and ``mtl_backward`` functions, which
 are meant to replace the call to ``torch.backward`` or ``loss.backward`` in gradient descent. To
 combine the information of the Jacobian, an aggregator from the ``aggregation`` package has to be
 used.
