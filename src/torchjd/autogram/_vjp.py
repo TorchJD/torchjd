4dc3f7c7b0130f8f481337cab0b2022e6a4b434f
Valérian Rey <valerian.rey@gmail.com>
2025-09-06 15:55:24 +0200

    Fix variable name

--- src/torchjd/autogram/_vjp.py
+++ src/torchjd/autogram/_vjp.py
@@ -99,13 +99,13 @@ class AutogradVJP(VJP):
         super().__init__(module)
         self.outputs = outputs
         self.mask = [output.requires_grad for output in self.outputs]
-        self.trainable_params, self.param_spec = tree_flatten(self.trainable_params)
+        self.flat_trainable_params, self.param_spec = tree_flatten(self.trainable_params)
 
     def __call__(self, grad_outputs: PyTree, _: PyTree) -> dict[str, Tensor]:
         flat_grad_outputs = tree_flatten(grad_outputs)[0]
         grads = torch.autograd.grad(
             [t for t, requires_grad in zip(self.outputs, self.mask) if requires_grad],
-            self.trainable_params,
+            self.flat_trainable_params,
             [t for t, requires_grad in zip(flat_grad_outputs, self.mask) if requires_grad],
             retain_graph=True,
             allow_unused=True,
