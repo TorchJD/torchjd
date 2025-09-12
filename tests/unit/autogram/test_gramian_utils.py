119e0058f113ecf8800677d6f37705ec353f293b
Pierre Quinton <pierre.quinton@gmail.com>
2025-09-12 07:42:48 +0200

    Improve `gramian_utils` tests.

--- tests/unit/autogram/test_gramian_utils.py
+++ tests/unit/autogram/test_gramian_utils.py
@@ -1,99 +1,78 @@
-from math import prod
-
 import torch
 from pytest import mark
 from torch import Tensor
 from torch.testing import assert_close
-from utils.tensors import rand_
+from utils.tensors import randn_
 
 from torchjd.autogram._gramian_utils import movedim_gramian, reshape_gramian
 
 
-def compute_quadratic_form(generalized_gramian: Tensor, x: Tensor) -> Tensor:
+def _compute_gramian(matrix: Tensor) -> Tensor:
     """
-    Compute the quadratic form x^T G x when the provided generalized Gramian and x may have multiple
-    dimensions.
+    Contracts the last dimension of matrix to make it into a Gramian.
     """
-    indices = list(range(x.ndim))
-    linear_form = torch.tensordot(x, generalized_gramian, dims=(indices, indices))
-    return torch.tensordot(linear_form, x, dims=(indices[::-1], indices))
+
+    indices = list(range(len(matrix.shape)))
+    transposed_matrix = matrix.movedim(indices, indices[::-1])
+    return torch.tensordot(matrix, transposed_matrix, dims=([-1], [0]))
 
 
 @mark.parametrize(
-    "shape",
+    ["original_shape", "target_shape"],
     [
-        [50, 2, 2, 3],
-        [60, 3, 2, 5],
-        [30, 6, 7],
-        [4, 3, 1],
-        [4, 1, 1],
-        [1, 1, 1],
-        [4, 1],
-        [4],
-        [1, 1],
-        [1],
+        ([], []),
+        ([], [1, 1]),
+        ([1], []),
+        ([12], [2, 3, 2]),
+        ([12], [4, 3]),
+        ([12], [12]),
+        ([4, 3], [12]),
+        ([4, 3], [2, 3, 2]),
+        ([4, 3], [3, 4]),
+        ([4, 3], [4, 3]),
+        ([6, 7, 9], [378]),
+        ([6, 7, 9], [9, 42]),
+        ([6, 7, 9], [2, 7, 27]),
+        ([6, 7, 9], [6, 7, 9]),
     ],
 )
-def test_quadratic_form_invariance_to_reshape(shape: list[int]):
-    """
-    When reshaping a Gramian, we expect it to represent the same quadratic form that now applies to
-    reshaped inputs. So the mapping x -> x^T G x commutes with reshaping x, G and then computing the
-    corresponding quadratic form.
-    """
+def test_gramian_equivariance_reshape(original_shape: list[int], target_shape: list[int]):
+    original_matrix = randn_(original_shape + [2])
+    target_matrix = original_matrix.reshape(target_shape + [2])
 
-    flat_dim = prod(shape[1:])
-    iterations = 20
+    original_gramian = _compute_gramian(original_matrix)
+    target_gramian = _compute_gramian(target_matrix)
 
-    matrix = rand_([flat_dim, shape[0]])
-    gramian = matrix @ matrix.T
-    reshaped_gramian = reshape_gramian(gramian, shape[1:])
+    reshaped_gramian = reshape_gramian(original_gramian, target_shape)
 
-    for _ in range(iterations):
-        vector = rand_([flat_dim])
-        reshaped_vector = vector.reshape(shape[1:])
-
-        quadratic_form = vector @ gramian @ vector
-        reshaped_quadratic_form = compute_quadratic_form(reshaped_gramian, reshaped_vector)
-
-        assert_close(reshaped_quadratic_form, quadratic_form)
+    assert_close(reshaped_gramian, target_gramian)
 
 
 @mark.parametrize(
     ["shape", "source", "destination"],
     [
-        ([50, 2, 2, 3], [0, 2], [1, 0]),
-        ([60, 3, 2, 5], [1], [2]),
-        ([30, 6, 7], [0, 1], [1, 0]),
-        ([4, 3, 1], [0, 1], [1, 0]),
-        ([4, 1, 1], [1], [0]),
-        ([1, 1, 1], [], []),
-        ([4, 1], [0], [0]),
-        ([4], [], []),
-        ([1, 1], [], []),
+        ([], [], []),
+        ([1], [0], [0]),
         ([1], [], []),
+        ([1, 1], [], []),
+        ([1, 1], [1], [0]),
+        ([6, 7], [1], [0]),
+        ([3, 1], [0, 1], [1, 0]),
+        ([1, 1, 1], [], []),
+        ([3, 2, 5], [], []),
+        ([1, 1, 1], [2], [0]),
+        ([3, 2, 5], [1], [2]),
+        ([2, 2, 3], [0, 2], [1, 0]),
+        ([2, 2, 3], [0, 2, 1], [1, 0, 2]),
     ],
 )
-def test_quadratic_form_invariance_to_movedim(
-    shape: list[int], source: list[int], destination: list[int]
-):
-    """
-    When moving dims on a Gramian, we expect it to represent the same quadratic form that now
-    applies to inputs with moved dims. So the mapping x -> x^T G x commutes with moving dims x, G
-    and then computing the quadratic form with those.
-    """
+def test_gramian_equivariance_movedim(shape: list[int], source: list[int], destination: list[int]):
+    original_matrix = randn_(shape + [2])
+    target_matrix = original_matrix.movedim(source, destination)
 
-    flat_dim = prod(shape[1:])
-    iterations = 20
+    original_gramian = _compute_gramian(original_matrix)
+    target_gramian = _compute_gramian(target_matrix)
 
-    matrix = rand_([flat_dim, shape[0]])
-    gramian = reshape_gramian(matrix @ matrix.T, shape[1:])
-    moved_gramian = movedim_gramian(gramian, source, destination)
+    moveddim_gramian = movedim_gramian(original_gramian, source, destination)
 
-    for _ in range(iterations):
-        vector = rand_(shape[1:])
-        moved_vector = vector.movedim(source, destination)
-
-        quadratic_form = compute_quadratic_form(gramian, vector)
-        moved_quadratic_form = compute_quadratic_form(moved_gramian, moved_vector)
-
-        assert_close(moved_quadratic_form, quadratic_form)
+    assert_close(moveddim_gramian, target_gramian)
