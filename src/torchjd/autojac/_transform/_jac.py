b2f41ea886115cc7e87cb4d6f0e3afc11a25472e
Valérian Rey <valerian.rey@gmail.com>
2025-09-13 17:23:14 +0200

    Stop reshaping Jacobians in Jac

--- src/torchjd/autojac/_transform/_jac.py
+++ src/torchjd/autojac/_transform/_jac.py
@@ -1,10 +1,9 @@
 import math
 from collections.abc import Callable, Sequence
 from functools import partial
-from itertools import accumulate
 
 import torch
-from torch import Size, Tensor
+from torch import Tensor
 
 from ._differentiate import Differentiate
 from ._materialize import materialize
@@ -69,7 +68,7 @@ class Jac(Differentiate):
                 ]
             )
 
-        def _get_vjp(grad_outputs: Sequence[Tensor], retain_graph: bool) -> Tensor:
+        def _get_vjp(grad_outputs: Sequence[Tensor], retain_graph: bool) -> tuple[Tensor, ...]:
             optional_grads = torch.autograd.grad(
                 self.outputs,
                 self.inputs,
@@ -79,15 +78,16 @@ class Jac(Differentiate):
                 allow_unused=True,
             )
             grads = materialize(optional_grads, inputs=self.inputs)
-            return torch.concatenate([grad.reshape([-1]) for grad in grads])
+            return grads
 
         # If the jac_outputs are correct, this value should be the same for all jac_outputs.
         m = jac_outputs[0].shape[0]
         max_chunk_size = self.chunk_size if self.chunk_size is not None else m
         n_chunks = math.ceil(m / max_chunk_size)
 
-        # List of tensors of shape [k_i, n] where the k_i's sum to m
-        jac_matrix_chunks = []
+        # One tuple per chunk (i), with one value per input (j), of shape [k_i] + shape[j],
+        # where k_i is the number of rows in the chunk (the k_i's sum to m)
+        jacs_chunks: list[tuple[Tensor, ...]] = []
 
         # First differentiations: always retain graph
         get_vjp_retain = partial(_get_vjp, retain_graph=True)
@@ -95,27 +95,22 @@ class Jac(Differentiate):
             start = i * max_chunk_size
             end = (i + 1) * max_chunk_size
             jac_outputs_chunk = [jac_output[start:end] for jac_output in jac_outputs]
-            jac_matrix_chunks.append(_get_jac_matrix_chunk(jac_outputs_chunk, get_vjp_retain))
+            jacs_chunks.append(_get_jacs_chunk(jac_outputs_chunk, get_vjp_retain))
 
         # Last differentiation: retain the graph only if self.retain_graph==True
         get_vjp_last = partial(_get_vjp, retain_graph=self.retain_graph)
         start = (n_chunks - 1) * max_chunk_size
         jac_outputs_chunk = [jac_output[start:] for jac_output in jac_outputs]
-        jac_matrix_chunks.append(_get_jac_matrix_chunk(jac_outputs_chunk, get_vjp_last))
+        jacs_chunks.append(_get_jacs_chunk(jac_outputs_chunk, get_vjp_last))
 
-        jac_matrix = torch.vstack(jac_matrix_chunks)
-        lengths = [input.numel() for input in self.inputs]
-        jac_matrices = _extract_sub_matrices(jac_matrix, lengths)
-
-        shapes = [input.shape for input in self.inputs]
-        jacs = _reshape_matrices(jac_matrices, shapes)
-
-        return tuple(jacs)
+        n_inputs = len(self.inputs)
+        jacs = tuple(torch.cat([chunks[i] for chunks in jacs_chunks]) for i in range(n_inputs))
+        return jacs
 
 
-def _get_jac_matrix_chunk(
-    jac_outputs_chunk: list[Tensor], get_vjp: Callable[[Sequence[Tensor]], Tensor]
-) -> Tensor:
+def _get_jacs_chunk(
+    jac_outputs_chunk: list[Tensor], get_vjp: Callable[[Sequence[Tensor]], tuple[Tensor, ...]]
+) -> tuple[Tensor, ...]:
     """
     Computes the jacobian matrix chunk corresponding to the provided get_vjp function, either by
     calling get_vjp directly or by wrapping it into a call to ``torch.vmap``, depending on the shape
@@ -126,18 +121,7 @@ def _get_jac_matrix_chunk(
     chunk_size = jac_outputs_chunk[0].shape[0]
     if chunk_size == 1:
         grad_outputs = [tensor.squeeze(0) for tensor in jac_outputs_chunk]
-        gradient_vector = get_vjp(grad_outputs)
-        return gradient_vector.unsqueeze(0)
+        gradients = get_vjp(grad_outputs)
+        return tuple(gradient.unsqueeze(0) for gradient in gradients)
     else:
         return torch.vmap(get_vjp, chunk_size=chunk_size)(jac_outputs_chunk)
-
-
-def _extract_sub_matrices(matrix: Tensor, lengths: Sequence[int]) -> list[Tensor]:
-    cumulative_lengths = [*accumulate(lengths)]
-    start_indices = [0] + cumulative_lengths[:-1]
-    end_indices = cumulative_lengths
-    return [matrix[:, start:end] for start, end in zip(start_indices, end_indices)]
-
-
-def _reshape_matrices(matrices: Sequence[Tensor], shapes: Sequence[Size]) -> Sequence[Tensor]:
-    return [matrix.view((matrix.shape[0],) + shape) for matrix, shape in zip(matrices, shapes)]
