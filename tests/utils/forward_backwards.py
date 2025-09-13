eefaff38724d9ca24569692c1cef0594c40b1e5e
Valérian Rey <valerian.rey@gmail.com>
2025-09-13 19:28:09 +0200

    Revert breaking everything with sum change

--- tests/utils/forward_backwards.py
+++ tests/utils/forward_backwards.py
@@ -116,6 +116,6 @@ def compute_gramian_with_autograd(
 
     jacobians = vmap(get_vjp)(torch.diag(torch.ones_like(output)))
     jacobian_matrices = [jacobian.reshape([jacobian.shape[0], -1]) for jacobian in jacobians]
-    gramian = torch.sum(torch.stack([jacobian @ jacobian.T for jacobian in jacobian_matrices]))
+    gramian = sum([jacobian @ jacobian.T for jacobian in jacobian_matrices])
 
     return gramian
