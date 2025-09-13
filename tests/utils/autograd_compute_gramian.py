5c132be9744b56aaa25d0abad22b8aae547c8a0f
Valérian Rey <valerian.rey@gmail.com>
2025-09-13 19:15:47 +0200

    Add docstring to compute_gramian_with_autograd

--- tests/utils/autograd_compute_gramian.py
+++ tests/utils/autograd_compute_gramian.py
@@ -5,6 +5,11 @@ from torch import Tensor, nn, vmap
 def compute_gramian_with_autograd(
     output: Tensor, inputs: list[nn.Parameter], retain_graph: bool = False
 ) -> Tensor:
+    """
+    Computes the Gramian of the Jacobian of the outputs with respect to the inputs using vmapped
+    calls to the autograd engine.
+    """
+
     filtered_inputs = [input for input in inputs if input.requires_grad]
 
     def get_vjp(grad_outputs: Tensor) -> list[Tensor]:
