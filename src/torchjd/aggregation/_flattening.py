b0c92cc8cdd80a92b5c865195d67030477f1eee8
Valérian Rey <valerian.rey@gmail.com>
2025-09-25 15:43:47 +0200

    Improve documentation of Flattening

--- src/torchjd/aggregation/_flattening.py
+++ src/torchjd/aggregation/_flattening.py
@@ -8,10 +8,15 @@ from torchjd.autogram._gramian_utils import reshape_gramian
 
 class Flattening(GeneralizedWeighting):
     """
-    :class:`~torchjd.aggregation._weighting_bases.GeneralizedWeighting` flattening the Gramian,
-    extracting a vector of weights from it using a
+    :class:`~torchjd.aggregation._weighting_bases.GeneralizedWeighting` flattening the generalized
+    Gramian into a square matrix, extracting a vector of weights from it using a
     :class:`~torchjd.aggregation._weighting_bases.Weighting`, and returning the reshaped tensor of
     weights.
+
+    For instance, when applied to a generalized Gramian of shape ``[2, 3, 3, 2]``, it would flatten
+    it into a square Gramian matrix of shape ``[6, 6]``, apply the weighting on it to get a vector
+    of weights of shape ``[6]``, and then return this vector reshaped into a matrix of shape
+    ``[2, 3]``.
 
     :param weighting: The weighting to apply to the Gramian matrix.
     """
