07be8d64d4f6d622f18ef8036e8fbf73f7c68b64
Pierre Quinton <pierre.quinton@gmail.com>
2025-10-13 08:48:50 +0200

    Fix GramianAccumulator tests.

--- tests/unit/autogram/test_gramian_accumulator.py
+++ tests/unit/autogram/test_gramian_accumulator.py
@@ -1,8 +1,13 @@
 from pytest import mark
+from torch import nn
 from torch.testing import assert_close
 from utils.tensors import randn_, zeros_
 
 from torchjd.autogram._gramian_accumulator import GramianAccumulator
+
+
+class FakeModule(nn.Module):
+    pass
 
 
 @mark.parametrize(
@@ -16,9 +21,10 @@ def test_adding_jacobians_one_by_one(shapes: list[list[int]], number_of_jacobian
     batch_size = 10
     gramian_accumulator = GramianAccumulator()
 
-    keys = [randn_(shape) for shape in shapes]
+    keys = [FakeModule() for _ in shapes]
     for key, n in zip(keys, number_of_jacobians):
-        gramian_accumulator.track_parameter_paths([key] * n)
+        for _ in range(n):
+            gramian_accumulator.track_module_paths(key)
 
     expected_gramian = zeros_([batch_size, batch_size])
 
@@ -27,7 +33,7 @@ def test_adding_jacobians_one_by_one(shapes: list[list[int]], number_of_jacobian
         cumulated_jacobian = zeros_(batched_shape)
         for i in range(n):
             jacobian = randn_(batched_shape)
-            gramian_accumulator.accumulate_path_jacobians({key: jacobian})
+            gramian_accumulator.accumulate_path_jacobians(key, [jacobian])
             cumulated_jacobian += jacobian
         jacobian_matrix = cumulated_jacobian.reshape([batch_size, -1])
         expected_gramian.addmm_(jacobian_matrix, jacobian_matrix.T)
@@ -48,21 +54,29 @@ def test_adding_jacobians_lots_by_lots(shapes: list[list[int]]):
     batch_size = 10
     gramian_accumulator = GramianAccumulator()
 
-    keys = [randn_(shape) for shape in shapes]
-    for i in range(number_of_jacobians):
-        gramian_accumulator.track_parameter_paths(keys)
+    keys = [FakeModule() for _ in shapes]
+    for key in keys:
+        for i in range(number_of_jacobians):
+            gramian_accumulator.track_module_paths(key)
 
     expected_gramian = zeros_([batch_size, batch_size])
 
-    cumulated_jacobians = {key: zeros_([batch_size] + shape) for key, shape in zip(keys, shapes)}
+    cumulated_jacobians = {
+        key: [zeros_([batch_size] + shape)] * number_of_jacobians
+        for key, shape in zip(keys, shapes)
+    }
     for i in range(number_of_jacobians):
-        jacobians = {key: randn_([batch_size] + shape) for key, shape in zip(keys, shapes)}
-        gramian_accumulator.accumulate_path_jacobians(jacobians)
-        for key, jacobian in jacobians.items():
-            cumulated_jacobians[key] += jacobian
+        jacobian_dict = {
+            key: [randn_([batch_size] + shape) for _ in range(number_of_jacobians)]
+            for key, shape in zip(keys, shapes)
+        }
+        for key, jacobians in jacobian_dict.items():
+            gramian_accumulator.accumulate_path_jacobians(key, jacobian_dict[key])
+            cumulated_jacobians[key] = [a + b for a, b in zip(jacobians, cumulated_jacobians[key])]
     for cumulated_jacobian in cumulated_jacobians.values():
-        jacobian_matrix = cumulated_jacobian.reshape([batch_size, -1])
-        expected_gramian.addmm_(jacobian_matrix, jacobian_matrix.T)
+        for jacobian in cumulated_jacobian:
+            jacobian_matrix = jacobian.reshape([batch_size, -1])
+            expected_gramian.addmm_(jacobian_matrix, jacobian_matrix.T)
 
     gramian = gramian_accumulator.gramian
     assert_close(gramian, expected_gramian)
@@ -84,14 +98,15 @@ def test_internal_dicts_are_cleaned(shapes: list[list[int]], number_of_jacobians
     batch_size = 10
     gramian_accumulator = GramianAccumulator()
 
-    keys = [randn_(shape) for shape in shapes]
+    keys = [FakeModule() for shape in shapes]
     for key, n in zip(keys, number_of_jacobians):
-        gramian_accumulator.track_parameter_paths([key] * n)
+        for _ in range(n):
+            gramian_accumulator.track_module_paths(key)
 
     for key, shape, n in zip(keys, shapes, number_of_jacobians):
         batched_shape = [batch_size] + shape
         for i in range(n):
             jacobian = randn_(batched_shape)
-            gramian_accumulator.accumulate_path_jacobians({key: jacobian})
+            gramian_accumulator.accumulate_path_jacobians(key, [jacobian])
         assert key not in gramian_accumulator._summed_jacobians.keys()
         assert key not in gramian_accumulator._path_counter.keys()
