7d5ec94fde6b45caf0488059d7716f54044a4914
Valérian Rey <31951177+ValerianRey@users.noreply.github.com>
2025-10-20 04:17:14 +0200

    test(autogram): Improve code quality (#464)
    * Extract rng forking into contexts.py
    * Make _forward_pass do rng forking
    * Make _forward_pass take reduction parameter
    * Make forward_pass public
    * Use forward_pass in test_engine.py, stop reseeding (it's now done by forward_pass)
    * Make zipping strict in make_mse_loss_fn
    * Stop requiring params in autograd_gramian_forward_backward
    * Improve parameter order of autogram_forward_backward
    * Rename some variables
    * Factorize input and target creation into make_inputs_and_targets
    * Reorder some code

--- tests/utils/contexts.py
+++ tests/utils/contexts.py
@@ -1,4 +1,16 @@
-from contextlib import AbstractContextManager
-from typing import TypeAlias
+from collections.abc import Generator
+from contextlib import AbstractContextManager, contextmanager
+from typing import Any, TypeAlias
+
+import torch
+from device import DEVICE
 
 ExceptionContext: TypeAlias = AbstractContextManager[Exception | None]
+
+
+@contextmanager
+def fork_rng(seed: int = 0) -> Generator[Any, None, None]:
+    devices = [DEVICE] if DEVICE.type == "cuda" else []
+    with torch.random.fork_rng(devices=devices, device_type=DEVICE.type) as ctx:
+        torch.manual_seed(seed)
+        yield ctx
