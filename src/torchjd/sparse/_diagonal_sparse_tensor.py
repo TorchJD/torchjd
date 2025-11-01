c97612f8d7f9bd2128c20ddbe03cd267dbf10f92
Valérian Rey <valerian.rey@gmail.com>
2025-11-01 09:29:52 +0100

    Add hardtanh_backward_default

--- src/torchjd/sparse/_diagonal_sparse_tensor.py
+++ src/torchjd/sparse/_diagonal_sparse_tensor.py
@@ -713,6 +713,20 @@ def threshold_backward_default(
     return DiagonalSparseTensor(new_physical, grad_output.v_to_ps)
 
 
+@DiagonalSparseTensor.implements(aten.hardtanh_backward.default)
+def hardtanh_backward_default(
+    grad_output: DiagonalSparseTensor,
+    self: Tensor,
+    min_val: Tensor | int | float,
+    max_val: Tensor | int | float,
+) -> DiagonalSparseTensor:
+    if isinstance(self, DiagonalSparseTensor):
+        raise NotImplementedError()
+
+    new_physical = aten.hardtanh_backward.default(grad_output.physical, self, min_val, max_val)
+    return DiagonalSparseTensor(new_physical, grad_output.v_to_ps)
+
+
 @DiagonalSparseTensor.implements(aten.slice.Tensor)
 def slice_Tensor(
     t: DiagonalSparseTensor, dim: int, start: int | None, end: int | None, step: int = 1
