ba9bf21762d55483fbc06670698e78307e2fb725
Pierre Quinton <pierre.quinton@gmail.com>
2025-11-24 14:31:37 +0100

    Reduce range of basis values to make the test pass.

--- tests/unit/sparse/test_linalg.py
+++ tests/unit/sparse/test_linalg.py
@@ -18,8 +18,8 @@ from torchjd.sparse._linalg import hnf_decomposition
 def test_hnf_decomposition(shape: tuple[int, int], max_rank: int):
     # Generate a matrix A of desired shape and rank max_rank with high probability and lower
     # otherwise.
-    U = torch.randint(-50, 51, [shape[0], max_rank], dtype=torch.int64)
-    V = torch.randint(-50, 51, [max_rank, shape[1]], dtype=torch.int64)
+    U = torch.randint(-10, 11, [shape[0], max_rank], dtype=torch.int64)
+    V = torch.randint(-10, 11, [max_rank, shape[1]], dtype=torch.int64)
     A = U @ V
     H, U, V = hnf_decomposition(A)
 
