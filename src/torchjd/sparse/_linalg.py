35270e430ce4bb4bdb06dc6e021e9d8c5f30ba39
Pierre Quinton <pierre.quinton@gmail.com>
2025-12-05 08:56:44 +0100

    Remove solve_int

--- src/torchjd/sparse/_linalg.py
+++ src/torchjd/sparse/_linalg.py
@@ -4,28 +4,6 @@ from torch import Tensor
 # TODO: Implement in C everything in this file.
 
 
-def solve_int(A: Tensor, B: Tensor, tol=1e-9) -> Tensor | None:
-    """
-    Solve A X = B where A, B and X have integer dtype.
-    Return X if such a matrix exists and otherwise None.
-    """
-
-    A_ = A.to(torch.float64)
-    B_ = B.to(torch.float64)
-
-    try:
-        X = torch.linalg.solve(A_, B_)
-    except RuntimeError:
-        return None
-
-    X_rounded = X.round()
-    if not torch.all(torch.isclose(X, X_rounded, atol=tol)):
-        return None
-
-    # TODO: Verify that the round operation cannot fail
-    return X_rounded.to(torch.int64)
-
-
 def extended_gcd(a: int, b: int) -> tuple[int, int, int]:
     """
     Extended Euclidean Algorithm (Python integers).
