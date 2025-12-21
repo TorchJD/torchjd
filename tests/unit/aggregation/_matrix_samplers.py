896e55d300e01893336eb26a4c6362a345fe5cdb
Valérian Rey <valerian.rey@gmail.com>
2025-12-21 17:54:07 +0100

    test: Remove dtype arg from MatrixSampler

--- tests/unit/aggregation/_matrix_samplers.py
+++ tests/unit/aggregation/_matrix_samplers.py
@@ -8,22 +8,20 @@ from utils.tensors import randint_, randn_, randperm_, zeros_
 
 
 class MatrixSampler(ABC):
-    """Abstract base class for sampling matrices of a given shape, rank and dtype."""
+    """Abstract base class for sampling matrices of a given shape, rank."""
 
-    def __init__(self, m: int, n: int, rank: int, dtype: torch.dtype = DTYPE):
-        self._check_params(m, n, rank, dtype)
+    def __init__(self, m: int, n: int, rank: int):
+        self._check_params(m, n, rank)
         self.m = m
         self.n = n
         self.rank = rank
-        self.dtype = dtype
 
-    def _check_params(self, m: int, n: int, rank: int, dtype: torch.dtype) -> None:
+    def _check_params(self, m: int, n: int, rank: int) -> None:
         """Checks that the provided __init__ parameters are acceptable."""
 
         assert m >= 0
         assert n >= 0
         assert 0 <= rank <= min(m, n)
-        assert dtype in {torch.float32, torch.float64}
 
     @abstractmethod
     def __call__(self, rng: torch.Generator | None = None) -> Tensor:
@@ -35,24 +33,24 @@ class MatrixSampler(ABC):
     def __str__(self) -> str:
         return (
             f"{self.__class__.__name__.replace('MatrixSampler', '')}"
-            f"({self.m}x{self.n}r{self.rank}:{str(self.dtype)[6:]})"
+            f"({self.m}x{self.n}r{self.rank})"
         )
 
 
 class NormalSampler(MatrixSampler):
-    """Sampler for random normal matrices of shape [m, n] with provided rank and dtype."""
+    """Sampler for random normal matrices of shape [m, n] with provided rank."""
 
     def __call__(self, rng: torch.Generator | None = None) -> Tensor:
-        U = _sample_orthonormal_matrix(self.m, dtype=self.dtype, rng=rng)
-        Vt = _sample_orthonormal_matrix(self.n, dtype=self.dtype, rng=rng)
-        S = torch.diag(torch.abs(randn_([self.rank], dtype=self.dtype, generator=rng)))
+        U = _sample_orthonormal_matrix(self.m, dtype=DTYPE, rng=rng)
+        Vt = _sample_orthonormal_matrix(self.n, dtype=DTYPE, rng=rng)
+        S = torch.diag(torch.abs(randn_([self.rank], dtype=DTYPE, generator=rng)))
         A = U[:, : self.rank] @ S @ Vt[: self.rank, :]
         return A
 
 
 class StrongSampler(MatrixSampler):
     """
-    Sampler for random strongly stationary matrices of shape [m, n] with provided rank and dtype.
+    Sampler for random strongly stationary matrices of shape [m, n] with provided rank.
 
     Definition: A matrix A is said to be strongly stationary if there exists a vector 0 < v such
     that v^T A = 0.
@@ -61,25 +59,24 @@ class StrongSampler(MatrixSampler):
     orthogonal to v.
     """
 
-    def _check_params(self, m: int, n: int, rank: int, dtype: torch.dtype) -> None:
-        super()._check_params(m, n, rank, dtype)
+    def _check_params(self, m: int, n: int, rank: int) -> None:
+        super()._check_params(m, n, rank)
         assert 1 < m
         assert 0 < rank <= min(m - 1, n)
 
     def __call__(self, rng: torch.Generator | None = None) -> Tensor:
-        v = torch.abs(randn_([self.m], dtype=self.dtype, generator=rng))
+        v = torch.abs(randn_([self.m], dtype=DTYPE, generator=rng))
         U1 = normalize(v, dim=0).unsqueeze(1)
         U2 = _sample_semi_orthonormal_complement(U1, rng=rng)
-        Vt = _sample_orthonormal_matrix(self.n, dtype=self.dtype, rng=rng)
-        S = torch.diag(torch.abs(randn_([self.rank], dtype=self.dtype, generator=rng)))
+        Vt = _sample_orthonormal_matrix(self.n, dtype=DTYPE, rng=rng)
+        S = torch.diag(torch.abs(randn_([self.rank], dtype=DTYPE, generator=rng)))
         A = U2[:, : self.rank] @ S @ Vt[: self.rank, :]
         return A
 
 
 class StrictlyWeakSampler(MatrixSampler):
     """
-    Sampler for random strictly weakly stationary matrices of shape [m, n] with provided rank and
-    dtype.
+    Sampler for random strictly weakly stationary matrices of shape [m, n] with provided rank.
 
     Definition: A matrix A is said to be weakly stationary if there exists a vector 0 <= v, v != 0,
     such that v^T A = 0.
@@ -97,32 +94,31 @@ class StrictlyWeakSampler(MatrixSampler):
     stationary.
     """
 
-    def _check_params(self, m: int, n: int, rank: int, dtype: torch.dtype) -> None:
-        super()._check_params(m, n, rank, dtype)
+    def _check_params(self, m: int, n: int, rank: int) -> None:
+        super()._check_params(m, n, rank)
         assert 1 < m
         assert 0 < rank <= min(m - 1, n)
 
     def __call__(self, rng: torch.Generator | None = None) -> Tensor:
-        u = torch.abs(randn_([self.m], dtype=self.dtype, generator=rng))
+        u = torch.abs(randn_([self.m], dtype=DTYPE, generator=rng))
         split_index = randint_(1, self.m, [], generator=rng).item()
         shuffled_range = randperm_(self.m, generator=rng)
-        v = zeros_(self.m, dtype=self.dtype)
+        v = zeros_(self.m, dtype=DTYPE)
         v[shuffled_range[:split_index]] = normalize(u[shuffled_range[:split_index]], dim=0)
-        v_prime = zeros_(self.m, dtype=self.dtype)
+        v_prime = zeros_(self.m, dtype=DTYPE)
         v_prime[shuffled_range[split_index:]] = normalize(u[shuffled_range[split_index:]], dim=0)
         U1 = torch.stack([v, v_prime]).T
         U2 = _sample_semi_orthonormal_complement(U1, rng=rng)
         U = torch.hstack([U1, U2])
-        Vt = _sample_orthonormal_matrix(self.n, dtype=self.dtype, rng=rng)
-        S = torch.diag(torch.abs(randn_([self.rank], dtype=self.dtype, generator=rng)))
+        Vt = _sample_orthonormal_matrix(self.n, dtype=DTYPE, rng=rng)
+        S = torch.diag(torch.abs(randn_([self.rank], dtype=DTYPE, generator=rng)))
         A = U[:, 1 : self.rank + 1] @ S @ Vt[: self.rank, :]
         return A
 
 
 class NonWeakSampler(MatrixSampler):
     """
-    Sampler for a random non weakly-stationary matrices of shape [m, n] with provided rank and
-    dtype.
+    Sampler for a random non weakly-stationary matrices of shape [m, n] with provided rank.
 
     Obtaining such a matrix is done by sampling a positive u, and by then sampling a matrix A that
     has u as one of its left-singular vectors, with positive singular value s. Any 0 <= v, v != 0,
@@ -130,17 +126,17 @@ class NonWeakSampler(MatrixSampler):
     contradiction. A is thus not weakly stationary.
     """
 
-    def _check_params(self, m: int, n: int, rank: int, dtype: torch.dtype) -> None:
-        super()._check_params(m, n, rank, dtype)
+    def _check_params(self, m: int, n: int, rank: int) -> None:
+        super()._check_params(m, n, rank)
         assert 0 < rank
 
     def __call__(self, rng: torch.Generator | None = None) -> Tensor:
-        u = torch.abs(randn_([self.m], dtype=self.dtype, generator=rng))
+        u = torch.abs(randn_([self.m], dtype=DTYPE, generator=rng))
         U1 = normalize(u, dim=0).unsqueeze(1)
         U2 = _sample_semi_orthonormal_complement(U1, rng=rng)
         U = torch.hstack([U1, U2])
-        Vt = _sample_orthonormal_matrix(self.n, dtype=self.dtype, rng=rng)
-        S = torch.diag(torch.abs(randn_([self.rank], dtype=self.dtype, generator=rng)))
+        Vt = _sample_orthonormal_matrix(self.n, dtype=DTYPE, rng=rng)
+        S = torch.diag(torch.abs(randn_([self.rank], dtype=DTYPE, generator=rng)))
         A = U[:, : self.rank] @ S @ Vt[: self.rank, :]
         return A
 
