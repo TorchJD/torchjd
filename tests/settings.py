2bdfaa535ff20861baf0f33ea612830dfd8b0c3c
Valérian Rey <valerian.rey@gmail.com>
2025-12-21 18:38:23 +0100

    test: Improve settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -2,21 +2,24 @@ import os
 
 import torch
 
+_POSSIBLE_TEST_DEVICES = {"cpu", "cuda:0"}
+_POSSIBLE_TEST_DTYPES = {"float32", "float64"}
+
 try:
     _device_str = os.environ["PYTEST_TORCH_DEVICE"]
 except KeyError:
     _device_str = "cpu"  # Default to cpu if environment variable not set
 
-if _device_str != "cuda:0" and _device_str != "cpu":
-    raise ValueError(f"Invalid value of environment variable PYTEST_TORCH_DEVICE: {_device_str}")
+if _device_str not in _POSSIBLE_TEST_DEVICES:
+    raise ValueError(
+        f"Invalid value of environment variable PYTEST_TORCH_DEVICE: {_device_str}.\n"
+        f"Possible values: {_POSSIBLE_TEST_DEVICES}."
+    )
 
 if _device_str == "cuda:0" and not torch.cuda.is_available():
     raise ValueError('Requested device "cuda:0" but cuda is not available.')
 
 DEVICE = torch.device(_device_str)
-
-
-_POSSIBLE_TEST_DTYPES = {"float32", "float64"}
 
 try:
     _dtype_str = os.environ["PYTEST_TORCH_DTYPE"]
