87b66f85d4ce8d9faa1c24ae028ab16b18f41d12
Valérian Rey <valerian.rey@gmail.com>
2026-01-14 01:56:06 +0100

    Fix comment in accumulate_jacs that applied to accumulate_grads

--- src/torchjd/autojac/_accumulation.py
+++ src/torchjd/autojac/_accumulation.py
@@ -31,14 +31,13 @@ def accumulate_jacs(params: Iterable[Tensor], jacobians: Iterable[Tensor]) -> No
             param_.jac += jac
         else:
             # We do not clone the value to save memory and time, so subsequent modifications of
-            # the value of key.grad (subsequent accumulations) will also affect the value of
-            # gradients[key] and outside changes to the value of gradients[key] will also affect
-            # the value of key.grad. So to be safe, the values of gradients should not be used
+            # the value of key.jac (subsequent accumulations) will also affect the value of
+            # jacobians[key] and outside changes to the value of jacobians[key] will also affect
+            # the value of key.jac. So to be safe, the values of jacobians should not be used
             # anymore after being passed to this function.
             #
             # We do not detach from the computation graph because the value can have grad_fn
-            # that we want to keep track of (in case it was obtained via create_graph=True and a
-            # differentiable aggregator).
+            # that we want to keep track of (in case it was obtained via create_graph=True).
             param.__setattr__("jac", jac)
 
 
