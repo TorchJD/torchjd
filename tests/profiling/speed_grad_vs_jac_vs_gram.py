4910f73055cb41b798b3c6c88d45605d5875df3a
Valérian Rey <valerian.rey@gmail.com>
2026-01-19 22:54:44 +0100

    Add missing parenthesis

--- tests/profiling/speed_grad_vs_jac_vs_gram.py
+++ tests/profiling/speed_grad_vs_jac_vs_gram.py
@@ -139,7 +139,7 @@ def time_call(fn, init_fn=noop, pre_fn=noop, post_fn=noop, n_runs: int = 10) -> 
 
 
 def print_times(name: str, times: Tensor) -> None:
-    print(f"{name} times (avg = {times.mean():.5f}, std = {times.std():.5f}")
+    print(f"{name} times (avg = {times.mean():.5f}, std = {times.std():.5f})")
     print(times)
     print()
 
