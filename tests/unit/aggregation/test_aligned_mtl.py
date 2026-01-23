8653929a3dc90ed19ef88fbe1e0b9eeede3b0306
Rasa Khosrowshahi <36485345+rkhosrowshahi@users.noreply.github.com>
2026-01-23 12:11:53 -0500

    feat(aggregation): Add `scale_mode` parameter to `AlignedMTL` (#527)
    * Add scale_mode parameter to AlignedMTL and AlignedMTLWeighting
    * Add test coverage for this
    * Add changelog entry

--- tests/unit/aggregation/test_aligned_mtl.py
+++ tests/unit/aggregation/test_aligned_mtl.py
@@ -1,13 +1,20 @@
 import torch
-from pytest import mark
+from pytest import mark, raises
 from torch import Tensor
+from utils.tensors import ones_
 
 from torchjd.aggregation import AlignedMTL
 
 from ._asserts import assert_expected_structure, assert_permutation_invariant
 from ._inputs import scaled_matrices, typical_matrices
 
-scaled_pairs = [(AlignedMTL(), matrix) for matrix in scaled_matrices]
+aggregators = [
+    AlignedMTL(),
+    AlignedMTL(scale_mode="median"),
+    AlignedMTL(scale_mode="rmse"),
+]
+scaled_pairs = [(aggregator, matrix) for aggregator in aggregators for matrix in scaled_matrices]
+# test_permutation_invariant seems to fail on gpu for scale_mode="median" or scale_mode="rmse".
 typical_pairs = [(AlignedMTL(), matrix) for matrix in typical_matrices]
 
 
@@ -23,9 +30,16 @@ def test_permutation_invariant(aggregator: AlignedMTL, matrix: Tensor):
 
 def test_representations():
     A = AlignedMTL(pref_vector=None)
-    assert repr(A) == "AlignedMTL(pref_vector=None)"
+    assert repr(A) == "AlignedMTL(pref_vector=None, scale_mode='min')"
     assert str(A) == "AlignedMTL"
 
     A = AlignedMTL(pref_vector=torch.tensor([1.0, 2.0, 3.0], device="cpu"))
-    assert repr(A) == "AlignedMTL(pref_vector=tensor([1., 2., 3.]))"
+    assert repr(A) == "AlignedMTL(pref_vector=tensor([1., 2., 3.]), scale_mode='min')"
     assert str(A) == "AlignedMTL([1., 2., 3.])"
+
+
+def test_invalid_scale_mode():
+    aggregator = AlignedMTL(scale_mode="test")  # type: ignore[arg-type]
+    matrix = ones_(3, 4)
+    with raises(ValueError, match=r"Invalid scale_mode=.*Expected"):
+        aggregator(matrix)
