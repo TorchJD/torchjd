aa64c4819c9c188ef5e9e4145799a07bdee23d50
Pierre Quinton <pierre.quinton@gmail.com>
2026-02-02 11:41:08 +0100

    make ty ignore `_nash_mtl.py` (this is now done in pyproject.toml

--- src/torchjd/aggregation/_nash_mtl.py
+++ src/torchjd/aggregation/_nash_mtl.py
@@ -23,7 +23,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# mypy: ignore-errors
 from typing import cast
 
 from torchjd._linalg import Matrix
